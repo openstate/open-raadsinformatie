c10d8aa753e6d5be2222c6e48644987daf5ff2f4
Sicco van Sas <sicco@openstate.eu>
2015-05-16 23:36:46 +0200

    Use ES host and port info from settings; increase scroll timeout; fix logical bug in while loop; some cleanup

--- ocd_backend/extractors/metadata.py
+++ ocd_backend/extractors/metadata.py
@@ -5,6 +5,7 @@ import json
 from ocd_backend.extractors import BaseExtractor, HttpRequestMixin
 from ocd_backend.extractors import log
 from ocd_backend.exceptions import NotFound
+from ocd_backend.settings import ELASTICSEARCH_HOST, ELASTICSEARCH_PORT
 
 
 class MetadataExtractor(BaseExtractor, HttpRequestMixin):
@@ -18,25 +19,31 @@ class MetadataExtractor(BaseExtractor, HttpRequestMixin):
         return r.text
 
     def get_metadata(self):
-        # Retrieve the PRIDs from the PRID index. Thi assumes that the
-        # PRID source has been processed before.
-        es = Elasticsearch(hosts='127.0.0.1:9200')
+        # Retrieve the PRIDs from the PRID index. This assumes that the
+        # PRID source has already been extracted.
+        es = Elasticsearch(
+            hosts='%s:%s' % (ELASTICSEARCH_HOST, ELASTICSEARCH_PORT)
+        )
+        #TODO change ocd_prid name once ocd is removed from index names
         scroll = es.search(
             index='ocd_prid',
-            scroll='1m',
+            scroll='5m',
             search_type='scan',
             fields='PRID'
         )
 
         scroll_id = scroll['_scroll_id']
-        scroll_size = scroll['hits']['total']
+        items_left = scroll['hits']['total']
 
-        while scroll_size > 0:
-            results = es.scroll(scroll_id=scroll_id, scroll='1m')
-            scroll_size = len(results['hits']['hits'])
+        while items_left > 0:
+            results = es.scroll(scroll_id=scroll_id, scroll='5m')
+            items_left -= len(results['hits']['hits'])
             for hit in results['hits']['hits']:
                 PRID = hit['fields']['PRID'][0]
                 metadata = self.call(PRID)
+                # Remove the function call 'parseMetadata(' and its
+                # closing parenthesis which are always part of the
+                # metadata
                 metadata = metadata.split("\n")[0][14:-1]
                 try:
                     metadata_json = json.loads(metadata)
@@ -44,37 +51,6 @@ class MetadataExtractor(BaseExtractor, HttpRequestMixin):
                     print 'Invalid metadata JSON: ' + str(e)
                     continue
                 yield metadata_json
-
-
-        #while date <= end_date:
-        #    print date.strftime("%Y-%m-%d")
-        #    items_left = True
-        #    current_page = 1
-
-        #    while items_left:
-        #        log.info('Getting npo items page %s', current_page)
-        #        sleep(1)
-        #        html = self.npo_call('zoeken')
-
-        #        PRIDs = {}
-        #        tree = etree.parse(StringIO(html), parser)
-        #        URLs = tree.xpath(
-        #            '//div[@class="list-item non-responsive row-fluid"]/div/' \
-        #            'a/@href'
-        #        )
-
-        #        if not URLs:
-        #            items_left = False
-        #        current_page += 1
-
-        #        for URL in URLs:
-        #            paths = URL.split('/')
-        #            PRIDs['PRID'] = paths[-1]
-        #            PRIDs['broadcast_date'] = paths[-2]
-        #            PRIDs['program_name'] = paths[-3]
-        #            yield PRIDs
-
-        #    date += self.delta
 
     def run(self):
         for item in self.get_metadata():
