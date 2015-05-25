a1a04b726c9b8f58fa2d093b8904eb92ee6c472b
Sicco van Sas <sicco@openstate.eu>
2015-05-26 01:49:08 +0200

    Extract more data for the PRID index

--- ocd_backend/extractors/prid.py
+++ ocd_backend/extractors/prid.py
@@ -3,6 +3,7 @@ from io import StringIO, BytesIO
 from time import sleep
 import datetime
 import json
+import re
 
 from ocd_backend.extractors import BaseExtractor, HttpRequestMixin
 from ocd_backend.extractors import log
@@ -28,6 +29,7 @@ class PRIDExtractor(BaseExtractor, HttpRequestMixin):
         end_date = datetime.date.today()
 
         parser = etree.HTMLParser()
+        base_xpath = '//div[@class="list-item non-responsive row-fluid"]'
 
         while date <= end_date:
             print date.strftime("%Y-%m-%d")
@@ -39,26 +41,78 @@ class PRIDExtractor(BaseExtractor, HttpRequestMixin):
                 sleep(1)
                 html = self.npo_call('zoeken', params={
                     'sort_date_period': date,
-                    'page': current_page
+                    'page': current_page,
+                    'view': 'tile'
                 })
-
-                PRIDs = {}
-                tree = etree.parse(StringIO(html), parser)
-                URLs = tree.xpath(
-                    '//div[@class="list-item non-responsive row-fluid"]/div/' \
-                    'a/@href'
-                )
-
-                if not URLs:
-                    items_left = False
                 current_page += 1
 
-                for URL in URLs:
+                tree = etree.parse(StringIO(html), parser)
+                items = tree.xpath(base_xpath)
+                if not items:
+                    items_left = False
+                    continue
+
+                for item in tree.xpath(base_xpath):
+                    data = {}
+
+                    # Retrieve dates of each program
+                    broadcast_date = item.xpath(
+                        'div[@class="span8"]/a/h5/text()'
+                    )[0]
+                    # The text field sometimes contains some extra info
+                    # besides the date; filter out just the date as the
+                    # other info is availabe in the metadata
+                    match = re.search(
+                        '(\w{2} \d{1,2} \w{3} \d{4} \d{2}:\d{2})',
+                        broadcast_date
+                    )
+                    data['broadcast_date'] = match.group(0)
+
+                    # Retrieve broadcaster(s) of each program
+                    broadcasters = []
+                    raw_broadcaster = item.xpath(
+                        'div[@class="span8"]/a/h4/span[@class="inactive"]'
+                        '/text()'
+                    )[0]
+                    # Remove parenthesis as the text looks like '(NOS)'
+                    raw_broadcaster = raw_broadcaster[1:-1]
+                    # Some programs are a collaboration between multiple
+                    # broadcasters, e.g. '(NOS, VARA en VPRO)'
+                    if ' en ' in raw_broadcaster:
+                        (head, tail) = raw_broadcaster.split(' en ')
+                        if ',' in head:
+                            for x in head.split(', '):
+                                broadcasters.append(x)
+                            broadcasters.append(tail)
+                        else:
+                            broadcasters.append(head)
+                            broadcasters.append(tail)
+                    else:
+                        broadcasters.append(raw_broadcaster)
+                    data['broadcasters'] = broadcasters
+
+                    # Retrieve program names
+                    data['program_name'] = ''.join(
+                        item.xpath('div[@class="span8"]/a/h4/text()')
+                    ).strip()
+
+                    # Retrieve program URLs listed on the webpage and
+                    # extract the PRID and program slug
+                    URL = item.xpath('div[@class="span8"]/a/@href')[0]
                     paths = URL.split('/')
-                    PRIDs['PRID'] = paths[-1]
-                    PRIDs['broadcast_date'] = paths[-2]
-                    PRIDs['program_name'] = paths[-3]
-                    yield PRIDs
+                    data['prid'] = paths[-1]
+                    data['program_slug'] = paths[-3]
+
+                    # Retrieve program image
+                    images = item.xpath(
+                        'div[@class="span4"]/div[@class="image-container"]/a/'
+                        'img/@data-images'
+                    )
+                    # Some programs don't have a image
+                    if images:
+                        data['image'] = eval(images[0])
+
+                    yield data
 
             date += self.delta
 
