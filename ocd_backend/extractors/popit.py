b694062d89685e5919c4d5044cfa76f676a21725
Breyten Ernsting <bje@dds.nl>
2016-07-28 16:39:53 +0200

    Fix a bug where the paginator would not yield items from page 2 and onwards due to inefficient programming.

--- ocd_backend/extractors/popit.py
+++ ocd_backend/extractors/popit.py
@@ -15,25 +15,23 @@ class PopItExtractor(StaticJSONExtractor, HttpRequestMixin):
         """
         Extracts items from the result of a popit call. Does paging.
         """
+
         static_json = json.loads(static_content)
+        page_count = 1
 
-        for item in static_json['result']:
-            print "%s (%s) - %s" % (
-                item.get('id', '-'), item.get('meta', {}).get('_type', '-'),
-                item.get('name', '-'),)
-            for mem in item.get('memberships', []):
-                print "-> %s (%s)" % (mem['organization_id'], mem['person_id'],)
-            yield 'application/json', json.dumps(item)
-
-        while static_json.get('next_url'):
-            result = self.http_session.get(
-                static_json['next_url'])
-            static_json = result.json()
-
+        while static_json is not None:
             for item in static_json['result']:
                 print "%s (%s) - %s" % (
                     item.get('id', '-'), item.get('meta', {}).get('_type', '-'),
                     item.get('name', '-'),)
                 for mem in item.get('memberships', []):
                     print "-> %s (%s)" % (mem['organization_id'], mem['person_id'],)
-                #yield 'application/json', json.dumps(item)
+                yield 'application/json', json.dumps(item)
+
+            if static_json.get('next_url'):
+                page_count += 1
+                result = self.http_session.get(
+                    static_json['next_url'])
+                static_json = result.json()
+            else:
+                static_json = None #force the end of the loop
