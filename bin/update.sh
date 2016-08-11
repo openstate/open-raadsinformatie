fbb7b9204ac4d2b1eada7b0f6b4003bd8e937eb1
Breyten Ernsting <bje@dds.nl>
2016-08-11 15:22:13 +0200

    Finish up.

--- bin/update.sh
+++ bin/update.sh
@@ -24,7 +24,7 @@ done
 for JSON_FILE in ocd_backend/sources/*.json
 do
   MUNI=`basename $JSON_FILE .json`
-  UPDATABLE_SOURCES="${MUNI}_committees ${MUNI}_popit_organizations ${MUNI}_popit_persons ${MUNI}_meetings ${MUNI}_reports ${MUNI}_resolutions ${MUNI}_videotulen ${MUNI}_motions"
+  UPDATABLE_SOURCES="${MUNI}_committees ${MUNI}_popit_organizations ${MUNI}_popit_persons ${MUNI}_meetings ${MUNI}_reports ${MUNI}_resolutions ${MUNI}_videotulen ${MUNI}_motions ${MUNI}_vote_events ${MUNI}_voting_rounds ${MUNI}_meeting_attendees"
 
   for UPDATE_SOURCE in $UPDATABLE_SOURCES
   do
