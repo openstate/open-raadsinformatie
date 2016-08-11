3a54ea989564d36cfbf67fcaea900c0d35945042
Breyten Ernsting <bje@dds.nl>
2016-08-11 15:08:11 +0200

    Add updater for iabsb to popit stuff.

--- bin/update.sh
+++ bin/update.sh
@@ -3,6 +3,24 @@
 source /opt/bin/activate
 cd /opt/ori
 
+# first only do ibabs to popit update
+for JSON_FILE in ocd_backend/sources/*.json
+do
+  MUNI=`basename $JSON_FILE .json`
+  # utrecht_ibabs_most_recent_popit_persons
+  UPDATABLE_SOURCES="${MUNI}_ibabs_most_recent_popit_persons ${MUNI}_ibabs_most_recent_popit_organizations ${MUNI}_ibabs_most_recent_popit_memberships ${MUNI}_ibabs_most_recent_popit_council_memberships"
+
+  for UPDATE_SOURCE in $UPDATABLE_SOURCES
+  do
+    echo `date` $UPDATE_SOURCE
+    ./manage.py extract start $UPDATE_SOURCE --sources_config=$JSON_FILE
+    sleep 60
+  done
+
+  sleep 60
+done
+
+# now do the other updates as well
 for JSON_FILE in ocd_backend/sources/*.json
 do
   MUNI=`basename $JSON_FILE .json`
