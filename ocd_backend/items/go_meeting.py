87a6c966611d116bc5161110ea6e98984e2efb5f
Breyten Ernsting <bje@dds.nl>
2016-10-05 15:01:22 +0200

    Apparently classifications are case sensitive now.

--- ocd_backend/items/go_meeting.py
+++ ocd_backend/items/go_meeting.py
@@ -47,7 +47,7 @@ class MeetingItem(
 
         results = self.api_request(
             self.source_definition['index_name'], 'organizations',
-            classification='council')
+            classification='Council')
         return results[0]
 
     def _find_meeting_type_id(self, org):
