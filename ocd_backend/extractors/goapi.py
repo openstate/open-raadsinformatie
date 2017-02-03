89378cb70e423e8eab6a529e64be3e56c59b1b6e
Jurrian Tromp <jurriantromp@gmail.com>
2017-02-03 14:57:35 +0100

    [Feature] GO API extract meetings and meetingitems by date interval
    Improved version of 91abffddbc077c3071dcad8cba41a610e78a9dbb

--- ocd_backend/extractors/goapi.py
+++ ocd_backend/extractors/goapi.py
@@ -1,4 +1,7 @@
 import json
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+from dateutil.parser import parse
 
 from ocd_backend.extractors import BaseExtractor, HttpRequestMixin
 
@@ -38,15 +41,47 @@ class GemeenteOplossingenMeetingsExtractor(GemeenteOplossingenBaseExtractor):
     """
 
     def run(self):
-        resp = self.http_session.get(
-            u'%s/meetings' % self.base_url)
+        current_start = datetime(2000, 1, 1)  # Start somewhere by default
+        if 'start_date' in self.source_definition:
+            current_start = parse(self.source_definition['start_date'])
 
-        if resp.status_code == 200:
-            static_json = json.loads(resp.content)
-            self.total = len(static_json)
+        end_date = datetime.today()  # End today by default
+        if 'end_date' in self.source_definition:
+            end_date = parse(self.source_definition['end_date'])
 
-            for meeting in static_json:
-                yield 'application/json', json.dumps(meeting)
+        months = 6  # Max 6 months intervals by default
+        if 'months_interval' in self.source_definition:
+            months = self.source_definition['months_interval']
+
+        print "Getting all meetings for %s to %s" % (current_start, end_date,)
+
+        meeting_count = 0
+        while True:
+            current_end = current_start + relativedelta(months=months)
+
+            # If next current_end exceeds end_date then stop at end_date
+            if current_end > end_date:
+                current_end = end_date
+                print "Next interval exceeds %s, months_interval not used" % end_date
+
+            resp = self.http_session.get(
+                u'%s/meetings?date_from=%i&date_to=%i' % (self.base_url, (current_start - datetime(1970, 1, 1)).total_seconds(), (current_end - datetime(1970, 1, 1)).total_seconds()))
+
+            if resp.status_code == 200:
+                static_json = json.loads(resp.content)
+                self.total = len(static_json)
+
+                for meeting in static_json:
+                    yield 'application/json', json.dumps(meeting)
+                    meeting_count += 1
+
+            print "Now processing meetings %s months from %s to %s" % (months, current_start, current_end,)
+            print "Extracted total of %d meetings." % (
+                meeting_count)
+
+            current_start = current_end + relativedelta(seconds=1)
+            if current_start > end_date:  # Stop while loop if exceeded end_date
+                break
 
 
 class GemeenteOplossingenMeetingItemsExtractor(
@@ -56,15 +91,47 @@ class GemeenteOplossingenMeetingItemsExtractor(
     """
 
     def run(self):
-        resp = self.http_session.get(
-            u'%s/meetings' % self.base_url)
+        current_start = datetime(2000, 1, 1)  # Start somewhere by default
+        if 'start_date' in self.source_definition:
+            current_start = parse(self.source_definition['start_date'])
 
-        if resp.status_code == 200:
-            static_json = json.loads(resp.content)
-            self.total = len(static_json)
+        end_date = datetime.today()  # End today by default
+        if 'end_date' in self.source_definition:
+            end_date = parse(self.source_definition['end_date'])
 
-            for meeting in static_json:
-                if 'items' in meeting:
-                    for item in meeting['items']:
-                        kv = {meeting['id']: item}
-                        yield 'application/json', json.dumps(kv)
+        months = 6  # Max 6 months intervals by default
+        if 'months_interval' in self.source_definition:
+            months = self.source_definition['months_interval']
+
+        print "Getting all meetingitems for %s to %s" % (current_start, end_date,)
+
+        meeting_count = 0
+        while True:
+            current_end = current_start + relativedelta(months=months)
+
+            # If next current_end exceeds end_date then stop at end_date
+            if current_end > end_date:
+                current_end = end_date
+                print "Next interval exceeds %s, months_interval not used" % end_date
+
+            resp = self.http_session.get(
+                u'%s/meetings?date_from=%i&date_to=%i' % (self.base_url, (current_start - datetime(1970, 1, 1)).total_seconds(), (current_end - datetime(1970, 1, 1)).total_seconds()))
+
+            if resp.status_code == 200:
+                static_json = json.loads(resp.content)
+                self.total = len(static_json)
+
+                for meeting in static_json:
+                    if 'items' in meeting:
+                        for item in meeting['items']:
+                            kv = {meeting['id']: item}
+                            yield 'application/json', json.dumps(kv)
+                            meeting_count += 1
+
+            print "Now processing meetingitems %s months from %s to %s" % (months, current_start, current_end,)
+            print "Extracted total of %d meetingsitems." % (
+                meeting_count)
+
+            current_start = current_end + relativedelta(seconds=1)
+            if current_start > end_date:  # Stop while loop if exceeded end_date
+                break
