f1636b2152dc7d99b625586e096561f3e6273c1f
Jurrian Tromp <jurriantromp@gmail.com>
2017-02-07 17:05:22 +0100

    [FIX] Failing motion normalizer test
    2016 != 2017

--- tests/ocd_backend/misc.py
+++ tests/ocd_backend/misc.py
@@ -1,15 +1,17 @@
 from unittest import TestCase
+import datetime
 
 from ocd_backend.utils.misc import normalize_motion_id
 
 class MotionIdNormalizerTestCase(TestCase):
     def test_normalize_motion_id(self):
+        year = datetime.datetime.now().isoformat()[0:4]
         normalized_motion_id = normalize_motion_id('2016 M 159')
         self.assertEqual(normalized_motion_id, '2016M159')
         normalized_motion_id = normalize_motion_id('2016 m 62')
         self.assertEqual(normalized_motion_id, '2016M62')
         normalized_motion_id = normalize_motion_id('M 124')
-        self.assertEqual(normalized_motion_id, '2016M124')
+        self.assertEqual(normalized_motion_id, '%sM124' % year)
         normalized_motion_id = normalize_motion_id('M 124', '2016-05-26T00:00:00+02:00')
         self.assertEqual(normalized_motion_id, '2016M124')
         normalized_motion_id = normalize_motion_id('M2016-67')
