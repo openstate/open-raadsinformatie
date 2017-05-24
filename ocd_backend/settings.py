f5d9bf4090a732322841a55b8c1c7e290d35b8d7
Jurrian Tromp <jurriantromp@gmail.com>
2017-05-24 15:36:54 +0200

    [FIX] Backend setting urls for querying the frontend

--- ocd_backend/settings.py
+++ ocd_backend/settings.py
@@ -68,7 +68,7 @@ COMBINED_INDEX = 'ori_combined_index'
 # The default prefix used for all data
 DEFAULT_INDEX_PREFIX = 'ori'
 
-RESOLVER_BASE_URL = 'http://localhost:5000/v0/resolve'
+RESOLVER_BASE_URL = 'http://frontend:5000/v0/resolve'
 RESOLVER_URL_INDEX = 'ori_resolver'
 
 # The User-Agent that is used when retrieving data from external sources
@@ -78,7 +78,7 @@ USER_AGENT = 'Open Raadsinformatie/0.1 (+http://www.openraadsinformatie.nl/)'
 # Should include API version and a trailing slash.
 # Can be overridden in the CLI when required, for instance when the user wants
 # to download dumps from another API instance than the one hosted by OpenState
-API_URL = 'http://127.0.0.1:5000/v0/'
+API_URL = 'http://frontend:5000/v0/'
 
 # The endpoint for the iBabs API
 IBABS_WSDL = u'https://www.mijnbabs.nl/iBabsWCFService/Public.svc?singleWsdl'
