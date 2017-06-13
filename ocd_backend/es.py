443687a3e823be324087cd29379d58b22fa3e467
Jurrian Tromp <jurriantromp@gmail.com>
2017-06-13 17:45:36 +0200

    [FIX] Removed Elasticsearch auth
    The Elasticsearch image contains premium xpack features including the security layer. However, the xpack license expires after a while, making ES unaccessable. Therefore security is disabled like it was before.

--- ocd_backend/es.py
+++ ocd_backend/es.py
@@ -4,7 +4,7 @@ from ocd_backend.settings import ELASTICSEARCH_HOST, ELASTICSEARCH_PORT
 
 
 def setup_elasticsearch(host=ELASTICSEARCH_HOST, port=ELASTICSEARCH_PORT):
-    return Elasticsearch([{'host': host, 'port': port}], http_auth=('elastic', 'changeme'))
+    return Elasticsearch([{'host': host, 'port': port}])
 
 
 elasticsearch = setup_elasticsearch()
