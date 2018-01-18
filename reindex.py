de89518c8f705d3750f2761facc9f10e4027dba0
Breyten Ernsting <bje@dds.nl>
2018-01-18 17:22:51 +0100

    Some auxilllary stuff.

---
+++ reindex.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+import requests
+import json
+
+indices = []
+with open('old-indices.txt') as in_file:
+    indices = [x.strip() for x in in_file.readlines()]
+
+for idx in indices:
+    data = {
+        "source": {
+            "remote":{"host": "http://c-open-raadsinformatie:9200"},
+            "index": idx,
+            "size": 25,
+        },
+        "dest": {
+            "index": idx
+        }
+    }
+    print requests.post('http://elasticsearch:9200/_reindex', data=json.dumps(data)).content
