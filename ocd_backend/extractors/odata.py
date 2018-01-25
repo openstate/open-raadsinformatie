14830dd9062b9f01e5eae935f5901245e2136522
Breyten Ernsting <bje@dds.nl>
2018-01-25 17:26:54 +0100

    Allow for a munucipality to have a different name (eg. CBS). Fixes #61

--- ocd_backend/extractors/odata.py
+++ ocd_backend/extractors/odata.py
@@ -1,4 +1,5 @@
 import json
+from pprint import pprint
 
 from .staticfile import StaticJSONExtractor
 
@@ -15,8 +16,10 @@ class ODataExtractor(StaticJSONExtractor):
         """
         static_json = json.loads(static_content)
         item_filter = self.source_definition['filter']
+        # print "Searching for: %s" % (item_filter,)
 
         for item in static_json['value']:
+            # pprint(item)
             passed_filter = (item_filter is None) or (
                 item[item_filter.keys()[0]] == item_filter.values()[0])
 
