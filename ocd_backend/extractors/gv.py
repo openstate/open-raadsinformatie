831f2761c9f7248ab40c6d6ee00b7df3f4ee8444
Breyten Ernsting <bje@dds.nl>
2018-05-11 17:07:13 +0200

    Add a meeting extractor.

--- ocd_backend/extractors/gv.py
+++ ocd_backend/extractors/gv.py
@@ -60,8 +60,41 @@ class GreenValleyExtractor(GreenValleyBaseExtractor):
         while fetch_next_page:
             print "Fetching items, starting from %s ..." % (params['start'],)
             results = self._fetch('GetObjectsByQuery', params).json()
-            pprint(results)
             for result in results['objects']:
                 yield 'application/json', json.dumps(result)
             params['start'] += len(results['objects'])
             fetch_next_page = (len(results['objects']) > 0)
+
+
+class GreenValleyMeetingsExtractor(GreenValleyExtractor):
+    def _get_meta(self):
+        return [
+            {
+                "metaname": "objecttype",
+                "operator": "=",
+                "type": "string",
+                "values": ["agenda"]
+            }, {
+                "metaname": "bis_vergaderdatum",
+                "operator": ">",
+                "type": "date",
+                "values": ["%s000" % (self.start_date.strftime('%s'),)]
+            }, {
+                "metaname": "bis_vergaderdatum",
+                "operator": "<",
+                "type": "date",
+                "values": ["%s000" % (self.end_date.strftime('%s'),)]
+            }
+        ]
+
+    def run(self):
+        self.start_date = None
+        self.end_date = None
+        for cur_start, cur_end in self.interval_generator():
+            if self.start_date is None:
+                self.start_date = cur_start
+            self.end_date = cur_end
+        log.info("%s: Now processing meetings from %s to %s" % (
+            self.source_definition['key'], self.start_date, self.end_date,))
+        for item in super(GreenValleyMeetingsExtractor, self).run():
+            yield item
