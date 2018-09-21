3bf9f096bcb6c5587d582c5bd6cee90d0105c95a
Jurrian Tromp <jurriantromp@gmail.com>
2018-09-21 13:38:59 +0200

    New partijgedrag data and transformer item

--- ocd_backend/items/partijgedrag.py
+++ ocd_backend/items/partijgedrag.py
@@ -1,5 +1,6 @@
 from ocd_backend.items import BaseItem
-from ocd_backend.models import Motion, Organization
+from ocd_backend.models import Motion, Organization, VoteEvent, ResultPass,\
+    ResultFail, Vote, Person, VoteOptionYes, VoteOptionNo, VoteOptionAbsent
 
 
 class PartijgedragMotion(BaseItem):
@@ -16,15 +17,56 @@ class PartijgedragMotion(BaseItem):
             'organization': 'ggm',
         }
 
-        object_model = Motion(self.original_item['id'], **source_defaults)
-        object_model.name = self.original_item['name']
-        object_model.text = self.original_item['text']
-        object_model.date = self.original_item['date']
-        object_model.legislative_session = self.original_item['legislative_session']
-        if self.original_item['creator']:
-            object_model.creator = [
-                Organization(x, **source_defaults) for x in self.original_item['creator'].split(',')
-            ]
-        object_model.organization = Organization('TK', **source_defaults)
+        motion = Motion(self.original_item['identifier'], **source_defaults)
+        motion.name = self.original_item.get('titel')
+        motion.text = self.original_item.get('tekst')
+        motion.date = self.original_item.get('issuedate')
+        motion.legislative_session = self.original_item['jaar']
+        motion.organization = Organization('TK', **source_defaults)
 
-        return object_model
+        if 'indieners' in self.original_item and len(self.original_item['indieners']) > 0:
+            indiener_id, indiener_naam = self.original_item['indieners'][0]
+            creator = Person(indiener_id, **source_defaults)
+            creator.name = indiener_naam
+            motion.creator = creator
+
+        if 'indieners' in self.original_item and len(self.original_item['indieners']) > 1:
+            motion.cocreator = list()
+            for indiener_id, indiener_naam in self.original_item['indieners'][1:]:
+                cocreator = Person(indiener_id, **source_defaults)
+                cocreator.name = indiener_naam
+                motion.cocreator.append(cocreator)
+
+        vote_event = VoteEvent(self.original_item['identifier'], **source_defaults)
+
+        if self.original_item['uitslag']:
+            vote_event.result = ResultPass()
+        elif not self.original_item['uitslag']:
+            vote_event.result = ResultFail()
+
+        if 'votes' in self.original_item:
+            votes = list()
+            for vote_option, vote_option_parties in self.original_item['votes'].items():
+                for vote_party in vote_option_parties:
+                    vote = Vote()
+
+                    if 'kamerlid' in vote_party:
+                        vote.voter = Person(vote_party['kamerlid'], **source_defaults)
+
+                    vote.group = Organization(vote_party['partij'], **source_defaults)
+                    vote.weight = vote_party['aantal']
+
+                    if vote_option == 'voor':
+                        vote.option = VoteOptionYes()
+                    elif vote_option == 'tegen':
+                        vote.option = VoteOptionNo()
+                    elif vote_option == 'afwezig':
+                        vote.option = VoteOptionAbsent()
+
+                    votes.append(vote)
+
+            vote_event.votes = votes
+
+        motion.vote_events = vote_event
+
+        return motion
