a406738e6e1e7e0e06eb7b01ac98755757548bc2
Jurrian Tromp <jurriantromp@gmail.com>
2018-09-21 13:42:04 +0200

    Remove .pyc files when dev mounting project to avoid running old code

--- bin/backend.sh
+++ bin/backend.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 cd /opt/ori
+find . -name "*.pyc" -exec rm -f {}
 celery flower --app=ocd_backend:celery_app &
 #celery worker --app=ocd_backend:celery_app --attach --pidfile=/var/run/celery/%n.pid --loglevel=info --concurrency=8
 celery multi start 4 -Q:1-2 default -Q slow -c8 -A ocd_backend:celery_app -l info --logfile=log/celery.log --pidfile=/var/run/celery/%n.pid
