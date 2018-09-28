7de3304997a78f9c88ef00b7fe7991e0dbda1c78
Breyten Ernsting <bje@dds.nl>
2018-09-28 15:53:57 +0200

    Different way of starting the extractor.

--- bin/update.sh
+++ bin/update.sh
@@ -3,11 +3,17 @@
 # source /opt/bin/activate
 cd /opt/ori
 
-./manage.py extract start ibabs
-./manage.py extract start go
-./manage.py extract start notubiz
-./manage.py extract start parlaeus
+./manage.py extract list_sources |awk 'NR > 1 {print $0}' |grep '\-s' |grep -v archive |grep -v 'zuid-holland' |cut -c 4- >.commands
 
+while IFS='' read -r line || [[ -n "$line" ]]; do
+    ./manage.py extract start $line
+done <.commands
+
+# ./manage.py extract start ibabs
+# ./manage.py extract start go
+# ./manage.py extract start notubiz
+# ./manage.py extract start parlaeus
+#
 ./manage.py extract start oude_ijsselstreek_committees
 ./manage.py extract start oude_ijsselstreek_popit_organizations
 ./manage.py extract start oude_ijsselstreek_popit_persons
@@ -18,6 +24,6 @@ cd /opt/ori
 ./manage.py extract start harderwijk_popit_organizations
 ./manage.py extract start harderwijk_popit_persons
 ./manage.py extract start harderwijk_videotulen
-
-./manage.py extract start go-provinces
-./manage.py extract start ibabs-provinces
+#
+# ./manage.py extract start go-provinces
+# ./manage.py extract start ibabs-provinces
