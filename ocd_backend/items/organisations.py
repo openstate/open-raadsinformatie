7f2d6dde695a7e962ba18487cc869b0056513952
Breyten Ernsting <bje@dds.nl>
2019-02-25 16:15:29 +0100

    Add yaml config file for GreenValley based information systems and add PoC for organizations and persons based on scraping HTML files.

--- ocd_backend/items/organisations.py
+++ ocd_backend/items/organisations.py
@@ -1,3 +1,5 @@
+import re
+
 from ocd_backend.items import BaseItem
 from ocd_backend.models import Organization
 
@@ -38,8 +40,37 @@ class AlmanakOrganisationItem(BaseItem):
             'organization': self.source_definition['key'],
         }
 
-        object_model = Organization(self.original_item['name'], **source_defaults)
+        object_model = Organization(
+            self.original_item['name'], **source_defaults)
         object_model.name = self.original_item['name']  # todo dubbel?
         object_model.classification = self.original_item['classification']
+        object_model.collection = self.get_collection()
+        return object_model
+
+
+class HTMLOrganisationItem(BaseItem):
+    def _get_name(self):
+        name = unicode(u''.join(self.original_item.xpath('.//text()'))).strip()
+        name = re.sub(r'\s*\(\d+ zetels?\)\s*', '', name)
+        return unicode(name)
+
+    def get_rights(self):
+        return u'undefined'
+
+    def get_collection(self):
+        return unicode(self.source_definition['index_name'])
+
+    def get_object_model(self):
+        source_defaults = {
+            'source': 'almanak',
+            'source_id_key': 'identifier',
+            'organization': self.source_definition['key'],
+        }
+
+        object_model = Organization(
+            self._get_name(), **source_defaults)
+        object_model.name = self._get_name()  # todo dubbel?
+        object_model.classification = unicode(
+            self.source_definition.get('classification', 'Party'))
         object_model.collection = self.get_collection()
         return object_model
