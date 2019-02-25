def3011b7c2bc255cbc2709c89d864bc8a378c23
Breyten Ernsting <bje@dds.nl>
2019-02-25 16:15:29 +0100

    Add yaml config file for GreenValley based information systems and add PoC for organizations and persons based on scraping HTML files.

--- ocd_backend/items/persons.py
+++ ocd_backend/items/persons.py
@@ -50,3 +50,53 @@ class AlmanakPersonItem(HttpRequestMixin, BaseItem):
         person.member_of = Relationship(municipality, rel=party)
 
         return person
+
+
+class HTMLPersonItem(HttpRequestMixin, BaseItem):
+    def _get_name(self):
+        return u''.join(
+            self.original_item.xpath(
+                self.source_definition.get(
+                    'persons_name_xpath', './/h2//text()'))).strip()
+
+    def get_rights(self):
+        return u'undefined'
+
+    def get_collection(self):
+        return unicode(self.source_definition['index_name'])
+
+    def get_object_model(self):
+        source_defaults = {
+            'source': 'almanak',
+            'source_id_key': 'identifier',
+            'organization': self.source_definition['key'],
+        }
+
+        html = self.original_item
+
+        person = Person(self._get_name(), **source_defaults)
+        person.name = self._get_name()
+
+        # TODO: fix the following fields
+        # person.email = html.xpath('string(//a[starts-with(@href,"mailto:")]/text())').strip().split(' ')[0]
+        # person.gender = u'male' if person.name.startswith(u'Dhr. ') else u'female'
+
+        # TODO: fix party membership
+        # municipality = Organization(self.source_definition['almanak_id'], **source_defaults)
+        # party = Organization(html.xpath('string(//ul[@class="definitie"]/li/ul/li)').strip(), **source_defaults)
+        #
+        # municipality_member = Membership()
+        # municipality_member.organization = municipality
+        # municipality_member.role = html.xpath('string(//div[@id="content"]//h3/text())').strip()
+        #
+        # party_member = Membership()
+        # party_member.organization = party
+        # party_member.role = 'Member'
+
+        # person.member_of = [municipality_member, party_member]
+
+        # person.member_of(municipality_member, party_member, rel=party)
+
+        # person.member_of = Relationship(municipality, rel=party)
+
+        return person
