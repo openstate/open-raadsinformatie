da6f07fbe5047cbee3f13f932940ed709de7a683
Breyten Ernsting <bje@dds.nl>
2019-02-27 16:13:39 +0100

    Save party meberships.

--- ocd_backend/items/persons.py
+++ ocd_backend/items/persons.py
@@ -1,3 +1,5 @@
+from urlparse import urljoin
+
 from lxml import etree
 
 from ocd_backend.items import BaseItem
@@ -77,28 +79,66 @@ class HTMLPersonItem(HttpRequestMixin, BaseItem):
         person = Person(self._get_name(), **source_defaults)
         person.name = self._get_name()
 
-        # TODO: fix the following fields
-        # person.email = html.xpath('string(//a[starts-with(@href,"mailto:")]/text())').strip().split(' ')[0]
+        return person
+
+
+class HTMLPersonFromLinkItem(HTMLPersonItem):
+    def get_object_model(self):
+        person = super(HTMLPersonFromLinkItem, self).get_object_model()
+
+        source_defaults = {
+            'source': 'almanak',
+            'source_id_key': 'identifier',
+            'organization': self.source_definition['key'],
+        }
+
+        # log.info(etree.tostring(self.original_item))
+        # log.info('Persons URL path: %s' % (self.source_definition['persons_link_path'],))
+        try:
+            request_url = urljoin(
+                self.source_definition['file_url'],
+                self.original_item.xpath(
+                    self.source_definition['persons_link_path'])[0])
+        except LookupError as e:
+            log.error(e)
+            return person
+
+        log.info('Now downloading URL: %s' % (request_url,))
+        r = self.http_session.get(request_url, verify=False)
+        r.raise_for_status()
+        html = etree.HTML(r.content)
+
+        try:
+            person.email = html.xpath(
+                'string(//a[starts-with(@href,"mailto:")]/text())').strip().split(' ')[0]
+        except LookupError:
+            pass
+
+        # TODO: not sure how to determine gender
         # person.gender = u'male' if person.name.startswith(u'Dhr. ') else u'female'
 
-        # municipality = Organization(
-        #     self.source_definition['almanak_id'], **source_defaults)
+        municipality = Organization(
+            self.source_definition['almanak_id'], **source_defaults)
+        if municipality is None:
+            log.debug('Could not find almanak organization')
+            return person
 
-        # party = Organization(
-        #     u''.join(self.original_item.xpath(
-        #         self.source_definition['organization_xpath'])),
-        #             **source_defaults)
+        party_name = u''.join(html.xpath(
+            self.source_definition['organization_xpath']))
+        log.info('Found party %s on personal page based on name' % (party_name,))
+        party = Organization(party_name, **source_defaults)
+        log.info('Found party %s on NEO4j' % (party,))
         #
-        # municipality_member = Membership()
-        # municipality_member.organization = municipality
-        # municipality_member.role = 'Fractielid'
+        municipality_member = Membership()
+        municipality_member.organization = municipality
+        municipality_member.role = 'Fractielid'
         # municipality_member.role = html.xpath('string(//div[@id="content"]//h3/text())').strip()
         #
-        # party_member = Membership()
-        # party_member.organization = party
-        # party_member.role = 'Member'
+        party_member = Membership()
+        party_member.organization = party
+        party_member.role = 'Member'
 
-        # person.member_of = [municipality_member, party_member]
+        person.member_of = [municipality_member, party_member]
 
         # person.member_of(municipality_member, party_member, rel=party)
 
