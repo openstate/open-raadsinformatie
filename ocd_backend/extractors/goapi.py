499756bfaa669504561d45db1ef3ac6676fa0305
Breyten Ernsting <bje@dds.nl>
2019-03-04 16:42:43 +0100

    Allow the GO extractor to work with both API versions.

--- ocd_backend/extractors/goapi.py
+++ ocd_backend/extractors/goapi.py
@@ -1,5 +1,7 @@
 import json
 from datetime import datetime
+from urlparse import urljoin
+from pprint import pprint
 
 from ocd_backend.extractors import BaseExtractor
 from ocd_backend.log import get_source_logger
@@ -20,7 +22,30 @@ class GemeenteOplossingenBaseExtractor(BaseExtractor, HttpRequestMixin):
     def __init__(self, *args, **kwargs):
         super(GemeenteOplossingenBaseExtractor, self).__init__(*args, **kwargs)
 
-        self.base_url = self.source_definition['base_url']
+        self.api_version = self.source_definition.get('api_version', 'v1')
+        self.base_url = '%s/%s/' % (
+            self.source_definition['base_url'], self.api_version,)
+
+    def _request(self, path):
+        log.info('Now retrieving: %s' % (urljoin(self.base_url, path),))
+        resp = self.http_session.get(
+            urljoin(self.base_url, path), verify=False)
+
+        if resp.status_code == 200:
+            static_json = json.loads(resp.content)
+            if self.api_version == 'v1':
+                return len(static_json), static_json
+            else:
+                parts = path.split('/')
+                if len(parts) > 1:
+                    model = parts[0]
+                else:
+                    model = path.split('?')[0]
+                return (
+                    int(static_json['result']['totalCount']),
+                    static_json['result'][model],)
+        else:
+            return (0, [],)
 
 
 class GemeenteOplossingenCommitteesExtractor(GemeenteOplossingenBaseExtractor):
@@ -29,15 +54,11 @@ class GemeenteOplossingenCommitteesExtractor(GemeenteOplossingenBaseExtractor):
     """
 
     def run(self):
-        resp = self.http_session.get(
-            u'%s/v1/dmus' % self.base_url)
-
-        if resp.status_code == 200:
-            static_json = json.loads(resp.content)
-            self.total = len(static_json)
-
-            for dmu in static_json:
-                yield 'application/json', json.dumps(dmu)
+        total, static_json = self._request('dmus')
+        pprint(static_json)
+        for dmu in static_json:
+            pprint(dmu)
+            yield 'application/json', json.dumps(dmu)
 
 
 class GemeenteOplossingenMeetingsExtractor(GemeenteOplossingenBaseExtractor):
@@ -48,59 +69,54 @@ class GemeenteOplossingenMeetingsExtractor(GemeenteOplossingenBaseExtractor):
     def run(self):
         meeting_count = 0
         for start_date, end_date in self.interval_generator():
-
-            resp = self.http_session.get(
-                u'%s/v1/meetings?date_from=%i&date_to=%i' % (
-                    self.base_url,
+            total, static_json = self._request(
+                'meetings?date_from=%i&date_to=%i' % (
                     (start_date - datetime(1970, 1, 1)).total_seconds(),
                     (end_date - datetime(1970, 1, 1)).total_seconds()
-                ), verify=False)
+                ))
 
-            if resp.status_code == 200:
-                static_json = json.loads(resp.content)
-
-                for meeting in static_json:
-                    yield 'application/json', json.dumps(meeting)
-                    meeting_count += 1
+            for meeting in static_json:
+                yield 'application/json', json.dumps(meeting)
+                meeting_count += 1
 
             log.info("Now processing meetings from %s to %s" % (start_date, end_date,))
             log.info("Extracted total of %d meetings." % meeting_count)
 
 
-class GemeenteOplossingenMeetingItemsExtractor(GemeenteOplossingenBaseExtractor):
-    """
-    Extracts meeting items from the GemeenteOplossingen API.
-    """
-
-    def run(self):
-        meeting_count = 0
-        for start_date, end_date in self.interval_generator():
-
-            resp = self.http_session.get(
-                u'%s/v1/meetings?date_from=%i&date_to=%i' % (
-                    self.base_url,
-                    (start_date - datetime(1970, 1, 1)).total_seconds(),
-                    (end_date - datetime(1970, 1, 1)).total_seconds()
-                )
-            )
-
-            if resp.status_code == 200:
-                static_json = json.loads(resp.content)
-
-                for meeting in static_json:
-                    if 'items' in meeting:
-                        for item in meeting['items']:
-
-                            # Temporary hack to inherit meetingitem date from meeting
-                            if 'date' not in item:
-                                item['date'] = meeting['date']
-
-                            kv = {meeting['id']: item}
-                            yield 'application/json', json.dumps(kv)
-                            meeting_count += 1
-
-            log.info("Now processing meetings from %s to %s" % (start_date, end_date,))
-            log.info("Extracted total of %d meetings." % meeting_count)
+# class GemeenteOplossingenMeetingItemsExtractor(GemeenteOplossingenBaseExtractor):
+#     """
+#     Extracts meeting items from the GemeenteOplossingen API.
+#     """
+#
+#     def run(self):
+#         meeting_count = 0
+#         for start_date, end_date in self.interval_generator():
+#
+#             resp = self.http_session.get(
+#                 u'%s/v1/meetings?date_from=%i&date_to=%i' % (
+#                     self.base_url,
+#                     (start_date - datetime(1970, 1, 1)).total_seconds(),
+#                     (end_date - datetime(1970, 1, 1)).total_seconds()
+#                 )
+#             )
+#
+#             if resp.status_code == 200:
+#                 static_json = json.loads(resp.content)
+#
+#                 for meeting in static_json:
+#                     if 'items' in meeting:
+#                         for item in meeting['items']:
+#
+#                             # Temporary hack to inherit meetingitem date from meeting
+#                             if 'date' not in item:
+#                                 item['date'] = meeting['date']
+#
+#                             kv = {meeting['id']: item}
+#                             yield 'application/json', json.dumps(kv)
+#                             meeting_count += 1
+#
+#             log.info("Now processing meetings from %s to %s" % (start_date, end_date,))
+#             log.info("Extracted total of %d meetings." % meeting_count)
 
 
 class GemeenteOplossingenDocumentsExtractor(GemeenteOplossingenBaseExtractor):
@@ -112,21 +128,14 @@ class GemeenteOplossingenDocumentsExtractor(GemeenteOplossingenBaseExtractor):
         meeting_count = 0
         for start_date, end_date in self.interval_generator():
 
-            resp = self.http_session.get(
+            total, docs = self._request(
                 u'%s/v2/documents?publicationDate_from=%s&publicationDate_to=%s&limit=50000' % (
-                    self.base_url,
                     start_date.isoformat(),
-                    end_date.isoformat()
-                ), verify=False
-            )
+                    end_date.isoformat()))
 
-            if resp.status_code == 200:
-                print resp.url
-                static_json = json.loads(resp.content)
-
-                for meeting in static_json['result']['documents']:
-                    yield 'application/json', json.dumps(meeting)
-                    meeting_count += 1
+            for doc in docs:
+                yield 'application/json', json.dumps(doc)
+                meeting_count += 1
 
             log.info("Now processing documents from %s to %s" % (start_date, end_date,))
             log.info("Extracted total of %d documents." % meeting_count)
