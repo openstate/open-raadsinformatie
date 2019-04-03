018318f9e52cf7f09d6264e7cb083dd7eca9652e
Jorrit van Belzen <jorrit787@gmail.com>
2019-04-03 14:48:35 +0200

    Improved Almanak Person extraction

--- ocd_backend/extractors/almanak.py
+++ ocd_backend/extractors/almanak.py
@@ -36,28 +36,53 @@ class OrganisationsExtractor(StaticHtmlExtractor):
 
 class PersonsExtractor(StaticHtmlExtractor):
     """
-    Extract persons from an Almanak
+    Extract persons from an Almanak.
     """
 
     def extract_items(self, static_content):
         """
-        Extracts persons from a HTML file. Also passes parties to the item
-        transformer.
+        Extracts persons from the Almanak page source HTML.
         """
 
         html = etree.HTML(static_content)
-        municipality = u''.join(html.xpath('//div[@id="content"]//h2/text()')).strip()
+        persons = []
 
-        for person in html.xpath('//ul[@class="definitie"][2]//ul//li//a'):
-            person_url = (u''.join(person.xpath('.//@href'))).strip()
-            person_text = (u''.join(person.xpath('.//text()'))).strip()
-            person_id = person_url[1:].split('/')[0]
+        for element in html.xpath('//*[@id="functies-organisatie"]/following::table[1]//tr'):
+            name = element.xpath('.//a/text()')[0].strip()
+            if name.lower() == 'vacant':
+                # Exclude vacant positions
+                break
 
-            if person_url != '':
-                # TODO: fields are not the best, but hey :)
-                yield 'application/json', json.dumps({
-                    'id': person_id,
-                    'url': person_url,
-                    'text': person_text,
-                    'municipality': municipality
-                })
+            try:
+                party = etree.tostring(element).split('(')[1].split(')')[0]
+            except:
+                # If no party can be extracted, the person is a clerk (griffier). Clerks are not
+                # affiliated with a party
+                party = None
+
+            url = (u''.join(element.xpath('.//@href'))).strip()
+            id = url[1:].split('/')[0]
+            email = element.xpath('string(//a[starts-with(@href,"mailto:")]/text())').strip().split(' ')[0]
+            gender = u'male' if name.startswith(u'Dhr. ') else u'female'
+
+            if party:
+                # Extraction of role requires a HTTP request to a separate page
+                request_url = u'https://almanak.overheid.nl%s' % (unicode(url),)
+                response = self.http_session.get(request_url, verify=False)
+                response.raise_for_status()
+                html = etree.HTML(response.content)
+                role = html.xpath('//*[@id="basis-medewerker"]/following::table[1]/tr/td/text()')[0].strip()
+            else:
+                role = 'Griffier'
+
+            persons.append({
+                'id': id,
+                'name': name,
+                'email': email,
+                'gender': gender,
+                'party': party,
+                'role': role,
+            })
+
+        for person in persons:
+            yield 'application/json', json.dumps(person)
