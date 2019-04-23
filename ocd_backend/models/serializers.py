0f5c68fb46057fd25119f6ec266fd1a77911fe08
Jurrian <jurrian@argu.co>
2019-04-23 15:39:01 +0200

    Override @context ori_identifier as resource @id. Fixes #93

--- ocd_backend/models/serializers.py
+++ ocd_backend/models/serializers.py
@@ -284,6 +284,7 @@ class JsonLDSerializer(JsonSerializer):
 
         deflated = self.deflate(model_object, props=True, rels=True)
         deflated['@context'] = {k: v for k, v in context.items() if k in deflated}
+        deflated['@context']['ori_identifier'] = '@id'
         deflated['@context']['@base'] = Ori.uri
         deflated['@context'][model_object.verbose_name()] = model_object.absolute_uri()
         deflated['@type'] = model_object.verbose_name()
