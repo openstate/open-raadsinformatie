039d215dcd56f6fd9ea0b8f831b6de0fb76620d0
Jurrian <jurrian@argu.co>
2019-05-04 10:30:30 +0200

    Added TemporaryFileMediaEnricher
    This MediaEnricher will not keep the downloaded files

--- ocd_backend/enrichers/media_enricher/static.py
+++ ocd_backend/enrichers/media_enricher/static.py
@@ -1,12 +1,12 @@
-import os
-
 from ocd_backend.enrichers.media_enricher import MediaEnricher
 from ocd_backend.log import get_source_logger
-from ocd_backend.settings import DATA_DIR_PATH
-from ocd_backend.utils.misc import get_sha1_hash
-from ocd_backend.utils.http import LocalCachingMixin, GCSCachingMixin
+from ocd_backend.utils.http import HttpRequestMixin, LocalCachingMixin, GCSCachingMixin
 
 log = get_source_logger('enricher')
+
+
+class TemporaryFileMediaEnricher(MediaEnricher, HttpRequestMixin):
+    pass
 
 
 class LocalStaticMediaEnricher(MediaEnricher, LocalCachingMixin):
