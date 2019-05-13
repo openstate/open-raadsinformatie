05442fd21baae673cc9897f700fac735b293f929
Jurrian <jurrian@argu.co>
2019-05-13 14:44:41 +0200

    Removing the manage.py dump commands

--- manage.py
+++ manage.py
@@ -3,24 +3,21 @@ from copy import deepcopy
 from datetime import datetime
 import json
 from glob import glob
-import gzip
 from hashlib import sha1
 import os
 import requests
 import sys
-from urlparse import urljoin
 
 import click
 from click.core import Command
 from click.decorators import _make_command
 
-from elasticsearch import helpers as es_helpers
 from elasticsearch.exceptions import RequestError
 
 from ocd_backend.es import elasticsearch as es
 from ocd_backend.pipeline import setup_pipeline
 from ocd_backend.settings import SOURCES_CONFIG_FILE, \
-    DEFAULT_INDEX_PREFIX, DUMPS_DIR, API_URL, LOCAL_DUMPS_DIR
+    DEFAULT_INDEX_PREFIX, DUMPS_DIR
 from ocd_backend.utils.misc import load_sources_config
 
 
@@ -381,262 +378,8 @@ def extract_start(source_id, subitem, entiteit, sources_config):
                 setup_pipeline(new_source)
 
 
-@command('runserver')
-@click.argument('host', default='0.0.0.0')
-@click.argument('port', default=5000, type=int)
-def frontend_runserver(host, port):
-    """
-    Run development server on ``host:port``.
-
-    :param host: host to run dev server on (defaults to 0.0.0.0)
-    :param port: defaults to 5000
-    """
-    from werkzeug.serving import run_simple
-    from ocd_frontend.wsgi import application
-    run_simple(host, port, application, use_reloader=True, use_debugger=True)
-
-
-@command('create')
-@click.option('--index', default=None)
-@click.pass_context
-def create_dump(ctx, index):
-    """
-    Create a dump of an index. If you don't provide an ``--index`` option,
-    you will be prompted with a list of available index names. Dumps are
-    stored as a gzipped txt file in ``settings.DUMPS_DIR/<index_name>/<
-    timestamp>_<index-name>.gz``, and a symlink ``<index-name>_latest.gz`` is
-    created, pointing to the last created dump.
-
-    :param ctx: Click context, so we can issue other management commands
-    :param index: name of the index you want to create a dump for
-    """
-    if not index:
-        available_idxs = ctx.invoke(available_indices)
-        if not available_idxs:
-            return
-        index = click.prompt('Name of index to dump')
-
-        if index not in available_idxs:
-            click.secho('"%s" is not an available index' % index, fg='red')
-            return
-
-    match_all = {'query': {'match_all': {}}}
-
-    total_docs = es.count(index=index).get('count')
-
-    path = _create_path(path=os.path.join(DUMPS_DIR, index))
-    dump_name = '%(index_name)s_%(timestamp)s.gz' % {
-        'index_name': index,
-        'timestamp': datetime.now().strftime('%Y%m%d%H%M%S')
-    }
-    new_dump = os.path.join(path, dump_name)
-
-    with gzip.open(new_dump, 'wb') as g:
-        with click.progressbar(es_helpers.scan(es, query=match_all, scroll='1m',
-                                               index=index),
-                               length=total_docs) as documents:
-            for doc in documents:
-                g.write('%s\n' % json.dumps(doc))
-
-    click.secho('Generating checksum', fg='green')
-    checksum = _checksum_file(new_dump)
-    checksum_path = os.path.join(DUMPS_DIR, index, '%s.sha1' % dump_name)
-
-    with open(checksum_path, 'w') as f:
-        f.write(checksum)
-
-    click.secho('Created dump "%s" (checksum %s)' % (dump_name, checksum),
-                fg='green')
-
-    latest = os.path.join(path, '%s_latest.gz' % index)
-    try:
-        os.unlink(latest)
-    except OSError:
-        click.secho('First time creating dump, skipping unlinking',
-                    fg='yellow')
-    os.symlink(new_dump, latest)
-    click.secho('Created symlink "%s_latest.gz" to "%s"' % (index, new_dump),
-                fg='green')
-
-    latest_checksum = os.path.join(os.path.dirname(checksum_path), '%s_latest.gz.sha1' % index)
-    try:
-        os.unlink(latest_checksum)
-    except OSError:
-        click.secho('First time creating dump, skipping unlinking checksum',
-                    fg='yellow')
-    os.symlink(checksum_path, latest_checksum)
-    click.secho('Created symlink "%s_latest.gz.sha1" to "%s"' % (index, checksum_path),
-                fg='green')
-
-
-@command('list')
-@click.option('--api-url', default=API_URL)
-def list_dumps(api_url):
-    """
-    List available dumps of API instance at ``api_address``. Use this option to
-    obtain information about dumps available at other OpenCultuurData API
-    instances.
-
-    :param api_url: URL of API location
-    """
-    url = urljoin(api_url, 'dumps')
-
-    try:
-        r = requests.get(url)
-    except:
-        click.secho('No OCD API instance with dumps available at {url}'
-                    .format(url=url), fg='red')
-        return
-
-    if not r.ok:
-        click.secho('Request on {url} failed'.format(url=url), fg='red')
-
-    dumps = r.json().get('dumps', {})
-    for index in dumps:
-        click.secho(index, fg='green')
-        for dump in sorted(dumps.get(index, []), reverse=True):
-            click.secho('\t{dump}'.format(dump=dump), fg='green')
-
-
-@command('download')
-@click.option('--api-url', default=API_URL, help='URL to API instance to fetch '
-                                                 'dumps from.')
-@click.option('--destination', '-d', default=LOCAL_DUMPS_DIR,
-              help='Directory to download dumps to.')
-@click.option('--collections', '-c', multiple=True)
-@click.option('--all-collections', '-a', is_flag=True, expose_value=True,
-              help='Download latest version of all collections available')
-def download_dumps(api_url, destination, collections, all_collections):
-    """
-    Download dumps of OCD collections to your machine, for easy ingestion.
-
-    :param api_url: URL to API instance to fetch dumps from. Defaults to ``ocd_frontend.settings.API_URL``, which is set to the API instance hosted by OpenCultuurData itself.
-    :param destination: path to local directory where dumps should be stored. Defaults to ``ocd_frontend.settings.LOCAL_DUMPS_DIR``.
-    :param collections: Names of collections to fetch dumps for. Optional; you will be prompted to select collections when not provided.
-    :param all_collections: If this flag is set, download all available dumps. Optional; you will be prompted to select collections when not provided.
-    """
-    url = urljoin(api_url, 'dumps')
-    try:
-        r = requests.get(url)
-    except:
-        click.secho('Could not connect to API', fg='red')
-        return
-
-    available_collections = [(i+1, index) for i, index in enumerate(r.json().get('dumps'))]
-    dumps = r.json().get('dumps')
-
-    if all_collections:
-        # Download all the things
-        for collection in dumps:
-            _download_dump([d for d in dumps.get(collection)
-                            if d.endswith('_latest.gz')][0],
-                           collection=collection,
-                           target_dir=destination)
-        return
-
-    if not collections:
-        for i, dump in available_collections:
-            click.secho('{i}) {index}'.format(i=i, index=dump), fg='yellow')
-
-        collection = click.prompt('For which collection do you want to download'
-                                  ' a dump? Please provide the number correspon'
-                                  'ding to the collection that you want to down'
-                                  'load', type=click.Choice([str(i[0]) for i in available_collections]))
-
-        collection = dict(available_collections)[int(collection)]
-        for i, dump in enumerate(dumps[collection]):
-            click.secho('{i}) {dump}'.format(i=i+1, dump=dump), fg='yellow')
-
-        dump_url = click.prompt('Which dump of the collection "{collection}" do'
-                                ' you want to download? Please provide the numb'
-                                'er corresponding to the dump that you want to '
-                                'download',
-                                type=click.Choice([str(j) for j in range(1, len(dumps[collection]) + 1)]))
-        dump_url = dumps[collection][int(dump_url)]
-
-        _download_dump(dump_url=dump_url, collection=collection,
-                       target_dir=destination)
-        return
-
-    for collection in collections:
-        if collection not in dumps.keys():
-            click.secho('"{}" is not available as a dump, skipping'.format(collection),
-                        fg='red')
-            continue
-
-        for i, dump in enumerate(dumps[collection]):
-            click.secho('{i}) {dump}'.format(i=i+1, dump=dump), fg='yellow')
-
-        dump_url = click.prompt('Which dump of the collection "{collection}" do'
-                                ' you want to download? Please provide the numb'
-                                'er corresponding to the dump that you want to '
-                                'download',
-                                type=click.Choice([str(j) for j in range(1, len(dumps[collection]) + 1)]))
-        dump_url = dumps[collection][int(dump_url)]
-
-        _download_dump(dump_url=dump_url, collection=collection,
-                       target_dir=destination)
-
-
-@command('load')
-@click.option('--collection-dump', '-c', help='Path to dump of collection to load',
-              default=None, type=click.Path(exists=True))
-@click.option('--collection-name', '-n', help='An index will be created with th'
-                                              'is name. If left empty, collecti'
-                                              'on name will be derived from dum'
-                                              'p name.', default=None)
-def load_dump(collection_dump, collection_name):
-    """
-    Restore an index from a dump file.
-
-    :param collection_dump: Path to a local gzipped dump to load.
-    :param collection_name: Name for the local index to restore the dump to. Optional; will be derived from the dump name, at your own risk. Note that the pipeline will add a "ocd_" prefix string to the collection name, to ensure the proper mapping and settings are applied.
-    """
-    available_dumps = glob(os.path.join(LOCAL_DUMPS_DIR, '*/*.gz'))
-    if not collection_dump:
-        choices = []
-        for i, dump in enumerate(available_dumps):
-            choices.append(unicode(i+1))
-            click.secho('{i}) {dump}'.format(i=i+1, dump=dump), fg='green')
-        dump_idx = click.prompt('Choose one of the dumps listed above',
-                                type=click.Choice(choices))
-        collection_dump = available_dumps[int(dump_idx) - 1]
-
-    collection = os.path.abspath(collection_dump)
-    collection_id = '_'.join(collection.split('/')[-1].split('.')[0].split('_')[:2])
-
-    if not collection_name:
-        collection_name = collection_id.replace('ocd_', '')
-
-    source_definition = {
-        'id': collection_id,
-        'extractor': 'ocd_backend.extractors.staticfile.StaticJSONDumpExtractor',
-        'transformer': 'ocd_backend.transformers.BaseTransformer',
-        'loader': 'ocd_backend.loaders.ElasticsearchLoader',
-        'cleanup': 'ocd_backend.tasks.CleanupElasticsearch',
-        'item': 'ocd_backend.items.LocalDumpItem',
-        'dump_path': collection,
-        'index_name': collection_name
-    }
-
-    click.secho(str(source_definition), fg='yellow')
-
-    setup_pipeline(source_definition)
-
-    click.secho('Queued items from {}. Please make sure your Celery workers'
-                ' are running, so the loaded items are processed.'.format(collection),
-                fg='green')
-
-
 # Register commands explicitly with groups, so we can easily use the docstring
 # wrapper
-frontend.add_command(frontend_runserver)
-
-dumps.add_command(load_dump)
-dumps.add_command(list_dumps)
-dumps.add_command(create_dump)
-dumps.add_command(download_dumps)
-
 elasticsearch.add_command(es_put_template)
 elasticsearch.add_command(es_put_mapping)
 elasticsearch.add_command(create_indexes)
