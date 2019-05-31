f0e7c206275fc2ff27aa98d3adee3c42d17cfcfd
Jurrian <jurrian@argu.co>
2019-05-31 17:37:47 +0200

    Fixes for CWC extractor

--- ocd_backend/extractors/cwc.py
+++ ocd_backend/extractors/cwc.py
@@ -1,4 +1,5 @@
 import json
+import os
 
 from suds.client import Client  # pylint: disable=import-error
 from suds.sudsobject import asdict  # pylint: disable=import-error
@@ -52,6 +53,8 @@ class CompanyWebcastBaseExtractor(BaseExtractor):
         super(CompanyWebcastBaseExtractor, self).__init__(*args, **kwargs)
 
         self.client = Client(settings.CWC_WSDL)
+        self.username = os.environ.get('%s.cwc_username' % self.source_definition['key'])
+        self.password = os.environ.get('%s.cwc_password' % self.source_definition['key'])
         # self.client.set_options(port='BasicHttpsBinding_IPublic')
 
 
@@ -62,47 +65,58 @@ class VideotulenExtractor(CompanyWebcastBaseExtractor):
 
     def run(self):
         current_page = 0
-        pagesize = self.source_definition['cwc_pagesize']
-        result_count = pagesize
+        consecutive_errors = 0
+        result_count = 0
+        pagesize = self.source_definition.get('cwc_pagesize', 10)
 
-        while result_count == pagesize:
-            log.debug("Requesting page %s ..." % (current_page,))
+        while consecutive_errors < 5:
+            log.debug("[%s] Requesting cwc page %s ..." % (self.source_definition['key'], current_page,))
             results = self.client.service.WebcastSearch(
-                Username=self.source_definition['cwc_username'],
-                Password=self.source_definition['cwc_password'],
-                PageSize=self.source_definition['cwc_pagesize'],
+                Username=self.username,
+                Password=self.password,
+                PageSize=pagesize,
                 PageNumber=current_page,
-                Order='CreatedDesc')
+                Order='CreatedDesc'
+            )
 
-            if results.WebcastSearchResult != 0:
-                log.warning("Page %s resulted in error code : %s" % (
-                    current_page, results.WebcastSearchResult,))
+            if results.WebcastSearchResult == 3:
+                log.warning("[%s] CompanyWebcast credentials are incorrect" % self.source_definition['key'])
+                break
+
+            elif results.WebcastSearchResult != 0:
+                log.warning("[%s] CompanyWebcast page %s resulted in error code : %s" % (
+                    self.source_definition['key'], current_page, results.WebcastSearchResult)
+                )
+
+                consecutive_errors += 1
+                current_page += 1
                 continue
 
-            result_count = 0
+            if len(results.WebcastSummaries) < 1:
+                break
+
             for result in results.WebcastSummaries[0]:
                 full_result = self.client.service.WebcastGet(
-                    Username=self.source_definition['cwc_username'],
-                    Password=self.source_definition['cwc_password'],
+                    Username=self.username,
+                    Password=self.password,
                     Code=result.Code)
 
                 result_count += 1
                 if full_result.WebcastGetResult != 0:
-                    log.warning("Webcast %s resulted in error code : %s" % (
-                        result.Code, full_result.WebcastGetResult,))
+                    log.warning("[%s] Webcast %s resulted in error code : %s" % (
+                        self.source_definition['key'], result.Code, full_result.WebcastGetResult,))
+                    consecutive_errors += 1
                     continue
 
-                if full_result is not None and full_result['Webcast'] is not None:
-                    log.debug("%s: %s" % (
-                        full_result['Webcast']['Title'],
-                        full_result['Webcast']['ScheduledStart'],))
+                # if full_result is not None and full_result['Webcast'] is not None:
+                #     log.debug("%s: %s" % (
+                #         full_result['Webcast']['Title'],
+                #         full_result['Webcast']['ScheduledStart'],))
 
                 serialized_result = suds_to_json(full_result)
                 yield 'application/json', serialized_result
-
-            if not self.source_definition['cwc_paging']:
-                result_count = 0
+                consecutive_errors = 0
 
             current_page += 1
 
-        log.info("[%s] Extracted total of %d CWC results" % (self.source_definition['index_name'], result_count))
+        log.info("[%s] Extracted total of %d cwc results" % (self.source_definition['index_name'], result_count))
