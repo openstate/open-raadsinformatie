2205d889ed1bca6be6b768632ba2d42a43b56fdd
Jurrian <jurrian@argu.co>
2019-06-07 13:16:41 +0200

    Removed extraction extensions

--- ocd_backend/extractors/extensions/__init__.py
+++
@@ -1,8 +0,0 @@
-from ocd_backend import celery_app
-# from ocd_backend import settings
-from ocd_backend.mixins import OCDBackendTaskFailureMixin
-
-
-class BaseExtension(OCDBackendTaskFailureMixin, celery_app.Task):
-    def run(self, *args, **kwargs):
-        raise NotImplementedError
