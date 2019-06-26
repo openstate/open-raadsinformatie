b7953d340eb452e12af522d7548bb986d3fd69d7
Jorrit <jorrit787@gmail.com>
2019-06-26 11:48:41 +0200

    Removed Neo4j-related tests

--- tests/ocd_backend/models/__init__.py
+++ tests/ocd_backend/models/__init__.py
@@ -1,5 +1,2 @@
 # Import test modules here so the noserunner can pick them up, and the
-# ModelsTestCase is parsed. Add additional testcases when required
-from .model import ModelTestCase
-from .database import DatabaseTestCase
 from .serializers import SerializersTestCase
