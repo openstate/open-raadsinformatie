5ed12a7664d93fb6158eef579f31f647a4657ef5
jorritb <jorrit787@gmail.com>
2019-06-27 14:44:27 +0200

    Fixed regression

--- tests/ocd_backend/models/__init__.py
+++ tests/ocd_backend/models/__init__.py
@@ -1,6 +1,3 @@
 # Import test modules here so the noserunner can pick them up, and the
 # ModelsTestCase is parsed. Add additional testcases when required
-from .model import ModelTestCase
-from .database import DatabaseTestCase
 from .postgres_database import PostgresDatabaseTestCase
-from .serializers import SerializersTestCase
