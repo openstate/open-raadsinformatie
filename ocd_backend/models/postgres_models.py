6405a3411199488f89d9f42bb342c8ef5f8d3d8b
jorritb <jorrit787@gmail.com>
2019-06-28 17:14:12 +0200

    Moved alembic.ini to Alembic folder; removed bare Excepts; re-did columns and migrations; TODO: create a new serializer that serializes properties as (predicate, value, property type)

--- ocd_backend/models/postgres_models.py
+++ ocd_backend/models/postgres_models.py
@@ -20,8 +20,7 @@ class Source(Base):
 class Resource(Base):
     __tablename__ = 'resource'
 
-    id = Column(Integer, Sequence('resource_id_seq'), primary_key=True)
-    ori_id = Column(Integer, Sequence('ori_id_seq'))
+    ori_id = Column(Integer, Sequence('ori_id_seq'), primary_key=True)
     iri = Column(String)
     source_iri_id = Column(Integer, ForeignKey("source.id"), nullable=False)
 
@@ -33,12 +32,14 @@ class Property(Base):
     __tablename__ = 'property'
 
     id = Column(UUIDType(), primary_key=True)
-    resource_id = Column(Integer, ForeignKey("resource.id"), nullable=False)
+    resource_id = Column(Integer, ForeignKey("resource.ori_id"), nullable=False)
     predicate = Column(String, nullable=False)
+    prop_resource = Column(Integer, ForeignKey("resource.ori_id"), nullable=True)
     prop_bool = Column(Boolean, nullable=True)
     prop_string = Column(String, nullable=True)
     prop_float = Column(Float, nullable=True)
     prop_datetime = Column(DateTime, nullable=True)
     prop_integer = Column(BigInteger, nullable=True)
+    prop_url = Column(String, nullable=True)
 
     resource = relationship("Resource", back_populates="properties", foreign_keys=resource_id)
