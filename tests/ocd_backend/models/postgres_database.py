132dd4e7b6376e2524d9e9706091f80e2b1d2d71
jorritb <jorrit787@gmail.com>
2019-07-05 14:58:39 +0200

    Dropped boolean and float columns from Source; Created PostgresSerializer to serialize model properties to a format usable in the Property table

--- tests/ocd_backend/models/postgres_database.py
+++ tests/ocd_backend/models/postgres_database.py
@@ -2,29 +2,3 @@ import datetime
 from unittest import TestCase
 
 from ocd_backend.models.postgres_database import PostgresDatabase
-
-
-class PostgresDatabaseTestCase(TestCase):
-
-    def test_map_column_type(self):
-        function = PostgresDatabase.map_column_type
-
-        values = (True, False)
-        for value in values:
-            self.assertEqual(function(value), 'prop_boolean')
-
-        values = (1, -4, 4024832)
-        for value in values:
-            self.assertEqual(function(value), 'prop_integer')
-
-        values = (1.5, -4.56, 402.4832)
-        for value in values:
-            self.assertEqual(function(value), 'prop_float')
-
-        values = (datetime.datetime.now().date(), datetime.datetime.now().date())
-        for value in values:
-            self.assertEqual(function(value), 'prop_datetime')
-
-        values = (str("Test string"), unicode("Test unicode string"), [1, 3, 5])
-        for value in values:
-            self.assertEqual(function(value), 'prop_string')
