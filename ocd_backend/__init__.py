1e84e0f0207a843b21d071ec3f76268ee54f37c6
jorritb <jorrit787@gmail.com>
2019-07-23 14:25:47 +0200

    Better way to register tasks with Celery

--- ocd_backend/__init__.py
+++ ocd_backend/__init__.py
@@ -6,17 +6,28 @@ from celery.utils import gen_task_name
 
 from ocd_backend.settings import CELERY_CONFIG
 
-celery_app = Celery('ocd_backend', include=[
-    'ocd_backend.pipeline',
-    'ocd_backend.models',
-    'ocd_backend.models.definitions',
-    'ocd_backend.extractors',
-    'ocd_backend.transformers',
-    'ocd_backend.enrichers.media_enricher',
-    'ocd_backend.enrichers.media_enricher.static',
-    'ocd_backend.loaders.elasticsearch',
-    'ocd_backend.loaders.delta',
-    'ocd_backend.tasks',
+celery_app = Celery('ocd_backend',
+    include=[
+        'ocd_backend.pipeline',
+        'ocd_backend.models',
+        'ocd_backend.models.definitions',
+        'ocd_backend.extractors',
+        'ocd_backend.transformers.goapi_committee',
+        'ocd_backend.transformers.goapi_document',
+        'ocd_backend.transformers.goapi_meeting',
+        'ocd_backend.transformers.gv',
+        'ocd_backend.transformers.ibabs_committee',
+        'ocd_backend.transformers.ibabs_meeting',
+        'ocd_backend.transformers.ibabs_person',
+        'ocd_backend.transformers.notubiz_committee',
+        'ocd_backend.transformers.notubiz_meeting',
+        'ocd_backend.transformers.organizations',
+        'ocd_backend.transformers.persons',
+        'ocd_backend.enrichers.media_enricher',
+        'ocd_backend.enrichers.media_enricher.static',
+        'ocd_backend.loaders.elasticsearch',
+        'ocd_backend.loaders.delta',
+        'ocd_backend.tasks',
 ])
 
 celery_app.conf.update(**CELERY_CONFIG)
