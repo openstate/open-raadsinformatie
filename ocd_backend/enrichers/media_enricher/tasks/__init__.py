d35ee91dba8737e1c8edcc35ccc22b6e67a77c71
Jurrian <jurrian@argu.co>
2019-11-08 14:46:17 +0100

    Add source_definition to BaseEnrichmentTask

--- ocd_backend/enrichers/media_enricher/tasks/__init__.py
+++ ocd_backend/enrichers/media_enricher/tasks/__init__.py
@@ -1,9 +1,12 @@
 class BaseEnrichmentTask(object):
     """The singleton base class that enrichment tasks should inherit."""
-    def __new__(cls):
+    def __new__(cls, source_definition):
         if not hasattr(cls, 'instance'):
-            cls.instance = super(BaseEnrichmentTask, cls).__new__(cls)
+            cls.instance = super(BaseEnrichmentTask, cls).__new__(cls, source_definition)
         return cls.instance
+
+    def __init__(self, source_definition):
+        self.source_definition = source_definition
 
     def enrich_item(self, item, file_object):
         raise NotImplementedError
