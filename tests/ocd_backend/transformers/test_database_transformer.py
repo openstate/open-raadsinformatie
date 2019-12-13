7112fbf9b1c10d9c40e252d29dca4059e3af8914
jorritb <jorrit787@gmail.com>
2019-12-13 12:09:39 +0100

    Added test for database transformer

--- tests/ocd_backend/transformers/test_database_transformer.py
+++ tests/ocd_backend/transformers/test_database_transformer.py
@@ -12,7 +12,7 @@ class DatabaseTransformerTestCase(TestCase):
         self.PWD = os.path.dirname(__file__)
         self.transformer_class = database_item
         self.transformer_class.get_supplier = self.mock_get_supplier
-        self.tested_models = set()
+        self.untested_models = set()
 
     @staticmethod
     def mock_get_supplier(ori_id):
@@ -22,6 +22,11 @@ class DatabaseTransformerTestCase(TestCase):
             return 'notubiz'
 
     def test_transformed_object_properties_equal(self):
+        """
+        Tests whether the properties of the transformed resource are equal to the properties in the JSON
+        dump. Note that date and datetime properties have been removed from the JSON because they are not
+        JSON serializable.
+        """
         with open(os.path.join(self.PWD, '../test_dumps/database_extracted_meeting.json'), 'r') as f:
             extracted_resource, extracted_subresources = json.loads(f.read())
         args = ('object', (extracted_resource, extracted_subresources), '612019', 'source_item_dummy')
@@ -32,32 +37,28 @@ class DatabaseTransformerTestCase(TestCase):
             }
         transformed_meeting = self.transformer_class.apply(args, kwargs).get()
 
+        # Test properties of the main resource
+        extracted_properties = {_prop['predicate']: _prop['value'] for _prop in extracted_resource['properties']
+                                if _prop['predicate'] != 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type'}
+        self.assertEqual(len(transformed_meeting.values), len(extracted_properties))
         for _property in transformed_meeting.values.iteritems():
-            try:
-                self.assertTrue(self.test_properties_equal(_property,
-                                                           transformed_meeting,
-                                                           extracted_resource,
-                                                           extracted_subresources))
-            except TypeError, e:
-                print e
-
-    def test_properties_equal(self, _property, resource, extracted_resource, extracted_subresources):
-        try:
             if isinstance(_property[1], Model):
-                if resource.ori_identifier not in self.tested_models:
-                    self.tested_models.add(resource.ori_identifier)
-                    for _subresource_property in resource.values.iteritems():
-                        self.test_properties_equal(_subresource_property,
-                                                   resource,
-                                                   extracted_resource,
-                                                   extracted_subresources)
+                self.untested_models.add(_property[1])
             else:
-                if resource.definition(_property[0]).absolute_uri() in \
-                        [_prop['predicate'] for _prop in extracted_resource['properties']]:
-                    return True
-                else:
-                    return False
-        except TypeError, e:
-            print e
+                self.assertTrue(transformed_meeting.definition(_property[0]).absolute_uri() in extracted_properties)
+                self.assertTrue(extracted_properties[transformed_meeting.definition(_property[0]).absolute_uri()] ==
+                                _property[1])
 
-# assert _property[1] == extracted_resource[transformed_meeting.definition(_property[0]).absolute_uri()]
+        # Test properties of subresources
+        for subresource in self.untested_models:
+            extracted_subresource_properties = extracted_subresources[subresource.ori_identifier.rsplit('/')[-1]][0]['properties']
+            extracted_properties = {_prop['predicate']: _prop['value'] for _prop in extracted_subresource_properties
+                                    if _prop['predicate'] != 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type'}
+            self.assertEqual(len(subresource.values), len(extracted_properties))
+            for _property in subresource.values.iteritems():
+                if isinstance(_property[1], Model):
+                    self.untested_models.add(_property[1])
+                else:
+                    self.assertTrue(subresource.definition(_property[0]).absolute_uri() in extracted_properties)
+                    self.assertTrue(extracted_properties[subresource.definition(_property[0]).absolute_uri()] ==
+                                    _property[1])
