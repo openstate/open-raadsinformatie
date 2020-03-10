openstate/open-raadsinformatie

commit e944f596617a3088a151454f7c1dd45851f9749d

Refactored GOAPI with a base transformer

ocd_backend/transformers/goapi_meeting.py
@@ -8,28 +8,29 @@ from ocd_backend.log import get_source_logger
 log = get_source_logger('goapi_meeting')
 
 
-def get_current_permalink(source_definition,original_item):
-    api_version = source_definition.get('api_version', 'v1')
-    base_url = '%s/%s' % (
-        source_definition['base_url'], api_version,)
+class GOAPITransformer(BaseTransformer):
+    def get_current_permalink(self, original_item):
+        api_version = self.source_definition.get('api_version', 'v1')
+        base_url = '%s/%s' % (
+            self.source_definition['base_url'], api_version,)
 
-    return u'%s/meetings/%i' % (base_url, original_item[u'id'],)
+        return u'%s/meetings/%i' % (base_url, original_item[u'id'],)
+
+    def get_documents_as_media_urls(self, original_item):
+        current_permalink = self.get_current_permalink(original_item)
+
+        output = []
+        for document in original_item['documents']:
+            # sleep(1)
+            url = u"%s/documents/%s" % (current_permalink, document['id'])
+            output.append({
+                'url': url,
+                'note': document[u'filename']})
+        return output
 
 
-def get_documents_as_media_urls(source_definition, original_item, documents):
-    current_permalink = get_current_permalink(source_definition, original_item)
-
-    output = []
-    for document in documents:
-        # sleep(1)
-        url = u"%s/documents/%s" % (current_permalink, document['id'])
-        output.append({
-            'url': url,
-            'note': document[u'filename']})
-    return output
-
-
-@celery_app.task(bind=True, base=BaseTransformer, autoretry_for=(Exception,), retry_backoff=True)
+# noinspection DuplicatedCode
+@celery_app.task(bind=True, base=GOAPITransformer, autoretry_for=(Exception,), retry_backoff=True)
 def meeting_item(self, content_type, raw_item, entity, source_item, **kwargs):
     original_item = self.deserialize_item(content_type, raw_item)
     self.source_definition = kwargs['source_definition']
@@ -133,7 +134,7 @@ def meeting_item(self, content_type, raw_item, entity, source_item, **kwargs):
         agendaitem.start_date = event.start_date
         agendaitem.attachment = []
 
-        for doc in get_documents_as_media_urls(self.source_definition, original_item, item.get('documents', [])):
+        for doc in self.get_documents_as_media_urls(original_item):
             attachment = MediaObject(doc['url'].rpartition('/')[2],
                                      self.source_definition,
                                      source=self.source_definition['key'],
@@ -154,7 +155,7 @@ def meeting_item(self, content_type, raw_item, entity, source_item, **kwargs):
         event.agenda.append(agendaitem)
 
     event.attachment = []
-    for doc in get_documents_as_media_urls(self.source_definition, original_item, original_item.get('documents', [])):
+    for doc in self.get_documents_as_media_urls(original_item):
         attachment = MediaObject(doc['url'].rpartition('/')[2],
                                  self.source_definition,
                                  source=self.source_definition['key'],

ocd_backend/transformers/goapi_document.py
@@ -2,35 +2,14 @@ import iso8601
 import pytz
 
 from ocd_backend import celery_app
-from ocd_backend.transformers import BaseTransformer
 from ocd_backend.models import *
 from ocd_backend.log import get_source_logger
+from ocd_backend.transformers.goapi_meeting import GOAPITransformer
 
 log = get_source_logger('goapi_document')
 
 
-def get_current_permalink(source_definition, original_item):
-    api_version = source_definition.get('api_version', 'v1')
-    base_url = '%s/%s' % (
-        source_definition['base_url'], api_version,)
-
-    return u'%s/documents/%i' % (base_url, original_item[u'id'],)
-
-
-def get_documents_as_media_urls(source_definition, original_item, documents):
-    current_permalink = get_current_permalink(source_definition, original_item)
-
-    output = []
-    for document in documents:
-        # sleep(1)
-        url = current_permalink
-        output.append({
-            'url': url,
-            'note': document[u'filename']})
-    return output
-
-
-@celery_app.task(bind=True, base=BaseTransformer, autoretry_for=(Exception,), retry_backoff=True)
+@celery_app.task(bind=True, base=GOAPITransformer, autoretry_for=(Exception,), retry_backoff=True)
 def document_item(self, content_type, raw_item, entity, source_item, **kwargs):
     original_item = self.deserialize_item(content_type, raw_item)
     self.source_definition = kwargs['source_definition']
@@ -75,7 +54,7 @@ def document_item(self, content_type, raw_item, entity, source_item, **kwargs):
     event.description = original_item[u'description']
 
     event.attachment = []
-    for doc in get_documents_as_media_urls(self.source_definition, original_item, original_item.get('documents', [])):
+    for doc in self.get_documents_as_media_urls(original_item):
         attachment = MediaObject(doc['url'],
                                  self.source_definition,
                                  **source_defaults)

commit d4ec998ea5ce3cbc7b5911379cdc9dee4a84ae8d

Bug fix incorrect Agenda item attachments related to #269 and #355
Bug was introduced in e944f596617a3088a151454f7c1dd45851f9749d

ocd_backend/transformers/goapi_meeting.py
@@ -17,11 +17,15 @@ class GOAPITransformer(BaseTransformer):
 
         return '%s/meetings/%i' % (base_url, original_item['id'],)
 
-    def get_documents_as_media_urls(self, original_item):
-        current_permalink = self.get_current_permalink(original_item)
+    def get_documents_as_media_urls(self, meeting, meeting_item=None):
+        item = meeting
+        if meeting_item:
+            item = meeting_item
+
+        current_permalink = self.get_current_permalink(meeting)
 
         output = []
-        for document in original_item.get('documents', []):
+        for document in item.get('documents', []):
             # sleep(1)
             url = "%s/documents/%s" % (current_permalink, document['id'])
             output.append({
@@ -134,7 +138,7 @@ def meeting_item(self, content_type, raw_item, canonical_iri, cached_path, **kwa
         agendaitem.last_discussed_at = event.start_date
         agendaitem.attachment = []
 
-        for doc in self.get_documents_as_media_urls(original_item):
+        for doc in self.get_documents_as_media_urls(original_item, item):
             attachment = MediaObject(doc['url'].rpartition('/')[2],
                                      source=self.source_definition['key'],
                                      supplier='gemeenteoplossingen',