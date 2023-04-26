33f1b27e9a1048905d0d52bcf7db594104a5929a
Breyten Ernsting <bje@dds.nl>
2023-04-26 15:23:12 +0200

    Basic setup script.

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+ORI_BACKEND_CONTAINER='ori_backend_1'
+ORI_POSTGRES_CONTAINER='ori_postgres_1'
+# elasticsearch mapping
+sudo docker exec $ORI_BACKEND_CONTAINER ./manage.py elasticsearch put_template
+# postgres User
+PG_USER="${POSTGRES_USERNAME:-ori_postgres_user}"
+PG_PASSWD="${POSTGRES_PASSWORD:-ori_postgres_password}"
+docker exec -it $ORI_POSTGRES_CONTAINER psql -U postgres -c "CREATE USER $PG_USER PASSWORD '$PG_PASSWD';"
+# create database
+PG_DB="${POSTGRES_DATABASEE:-ori}"
+docker exec -it $ORI_POSTGRES_CONTAINER psql -U postgres -c "CREATE DATABASE $PG_DB;"
+# postgres crypto extension hstore thingie
+docker exec -it $ORI_POSTGRES_CONTAINER psql -U postgres $PG_DB -c "CREATE EXTENSION pgcrypto;"
+# alembic migrations
+docker exec -it -w /opt/ori/ocd_backend/alembic $ORI_BACKEND_CONTAINER alembic upgrade head
