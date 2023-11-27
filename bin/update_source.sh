e109a5cd8f3d22853532ac16d50fc33b1bef3498
Breyten Ernsting <bje@dds.nl>
2023-11-27 15:12:12 +0100

    Add a script to update a municiplaity

---
+++ bin/update_source.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+FQPATH=`readlink -f $0`
+BINDIR=`dirname $FQPATH`
+cd $BINDIR/..
+tail -n +2 sources.txt >sources.tmp
+MUNI=`head -1 sources.txt`
+if [ "$MUNI" != "" ];
+then
+  echo sudo docker exec ori_backend_1 ./manage.py extract process daily --source_path "*"$MUNI
+fi
+cp sources.tmp sources.txt
