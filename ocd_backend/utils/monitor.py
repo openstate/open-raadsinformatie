a209095f99bb817549667fea46ed6f429795881a
Subsidiebot Amsterdam <subsidiebot@bje.dds.nl>
2024-09-18 15:00:32 +0200

    Add rudimentary monitoring tools. Fixes #321

---
+++ ocd_backend/utils/monitor.py
@@ -0,0 +1,41 @@
+#!usr/bin/env python
+from pprint import pprint
+
+import requests
+import json
+
+from ocd_backend.es import elasticsearch as es
+from ocd_backend.es import setup_elasticsearch
+
+def get_indices():
+    return [l.strip().split(" ")[2] for l in es.cat.indices().split("\n") if l != '']
+
+def get_recent_counts():
+    payload = {
+        'query': {
+            'bool': {
+                'filter': [
+                    {'term': {"@type": "MediaObject"}},
+                    {'range': {
+                        'last_discussed_at': {
+                            'gte': 'now-3000d'
+                        }
+                    }}
+                ]
+            }
+        },
+        "aggs": {
+          "index": {
+            "terms": {
+                "field": "_index"
+            }
+          }
+        },
+        "size": 0
+    }
+    resp = es.search(body=payload)
+    indices = get_indices()
+    result = {i: 0 for i in indices}
+    for b in resp.get('aggregations', {}).get('index', {}).get('buckets', []):
+        result[b['key']] = b['doc_count']
+    return result
