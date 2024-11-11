e52e5cb6684b09b7e959cdc7c122d6178c9c20c3
Rob van Dijk <697696+robvandijk@users.noreply.github.com>
2024-11-11 15:45:42 +0100

    Sets the correct Celery queues in backend.sh

--- bin/backend.sh
+++ bin/backend.sh
@@ -4,11 +4,11 @@ cd /opt/ori
 find . -name "*.pyc" -exec rm -f {}
 celery flower --app=ocd_backend.app:celery_app &
 #celery worker --app=ocd_backend.app:celery_app --attach --pidfile=/var/run/celery/%n.pid --loglevel=info --concurrency=8
-celery multi start 4 -Q:1-2 default -Q slow -c8 -A ocd_backend.app:celery_app -l info --logfile=log/celery.log --pidfile=/var/run/celery/%n.pid
+celery multi start 4 -Q transformers,enrichers -c8 -A ocd_backend.app:celery_app -l debug --logfile=log/celery.log --pidfile=/var/run/celery/%n.pid
 while true
   do
     inotifywait -e modify,attrib,close_write,move,delete -r /opt/ori/ocd_backend \
       && date \
-      && celery multi restart 4 -Q:1-2 default -Q slow -c8 -A ocd_backend.app:celery_app -l info --logfile=log/celery.log --pidfile=/var/run/celery/%n.pid
+      && celery multi restart 4 -Q transformers,enrichers -c8 -A ocd_backend.app:celery_app -l debug --logfile=log/celery.log --pidfile=/var/run/celery/%n.pid
     sleep 10
   done
