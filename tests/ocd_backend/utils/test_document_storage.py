f8efeae2c67c3a2ac0077ae971e1628f851dfdc6
Rob van Dijk <697696+robvandijk@users.noreply.github.com>
2024-12-19 18:11:27 +0100

    Fixes logic of DocumentStorage._id_partitioned

--- tests/ocd_backend/utils/test_document_storage.py
+++ tests/ocd_backend/utils/test_document_storage.py
@@ -4,17 +4,25 @@ from ocd_backend.utils.document_storage import DocumentStorage
 
 
 class DocumentStorageTestCase(TestCase):
-    def test_tiny_id(self):
-        self.assertEqual(DocumentStorage._id_partitioned(5), '000/000/005')
+    def test_normal_ids(self):
+        self.assertEqual(DocumentStorage._id_partitioned(345), '000/000/000')
+        self.assertEqual(DocumentStorage._id_partitioned(6930), '000/000/006')
+        self.assertEqual(DocumentStorage._id_partitioned(49630), '000/000/049')
+        self.assertEqual(DocumentStorage._id_partitioned(634291), '000/000/634')
+        self.assertEqual(DocumentStorage._id_partitioned(957241), '000/000/957')
+        self.assertEqual(DocumentStorage._id_partitioned(1000365), '000/001/000')
+        self.assertEqual(DocumentStorage._id_partitioned(1506895), '000/001/506')
+        self.assertEqual(DocumentStorage._id_partitioned(5849348398), '005/849/348')
 
-    def test_small_id(self):
-        self.assertEqual(DocumentStorage._id_partitioned(847), '000/000/847')
+    def test_edge_cases(self):
+        self.assertEqual(DocumentStorage._id_partitioned(1), '000/000/000')
+        self.assertEqual(DocumentStorage._id_partitioned(999), '000/000/000')
+        self.assertEqual(DocumentStorage._id_partitioned(1000), '000/000/001')
+        self.assertEqual(DocumentStorage._id_partitioned(1001), '000/000/001')
+        self.assertEqual(DocumentStorage._id_partitioned(2000), '000/000/002')
+        self.assertEqual(DocumentStorage._id_partitioned(2001), '000/000/002')
+        self.assertEqual(DocumentStorage._id_partitioned(999999), '000/000/999')
+        self.assertEqual(DocumentStorage._id_partitioned(1000000), '000/001/000')
 
-    def test_medium_id(self):
-        self.assertEqual(DocumentStorage._id_partitioned(13823), '000/013/823')
-
-    def test_large_id(self):
-        self.assertEqual(DocumentStorage._id_partitioned(431382623), '431/382/623')
-
-    def test_too_large_id(self):
-        self.assertEqual(DocumentStorage._id_partitioned(2431382623), '243/138/2623')
+        self.assertEqual(DocumentStorage._id_partitioned(999999999), '000/999/999')
+        self.assertEqual(DocumentStorage._id_partitioned(1000000000), '001/000/000')
