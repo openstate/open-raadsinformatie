90a5baed239823eb0977820699e833a146ac2186
Rob van Dijk <697696+robvandijk@users.noreply.github.com>
2025-01-07 17:15:24 +0100

    Sets start_date to day before yesterday to (again) retry failed jobs

--- bin/update.sh
+++ bin/update.sh
@@ -2,10 +2,10 @@
 FQPATH=`readlink -f $0`
 BINDIR=`dirname $FQPATH`
 TODAY=`date +%Y-%m-%d`
-YESTERDAY=`date -d yesterday +%Y-%m-%d`
+DAY_BEFORE_YESTERDAY=`date -d "-2 day" +%Y-%m-%d`
 cd $BINDIR/..
 sudo docker exec ori_redis_1 redis-cli --scan --pattern 'pipeline_*' |sudo xargs docker exec ori_redis_1 redis-cli del
-sudo docker exec ori_redis_1 redis-cli -n 1 set _daily.start_date "$YESTERDAY"
+sudo docker exec ori_redis_1 redis-cli -n 1 set _daily.start_date "$DAY_BEFORE_YESTERDAY"
 sudo docker exec ori_redis_1 redis-cli -n 1 set _daily.end_date "$TODAY"
 sudo docker exec ori_backend_1 ./manage.py extract load_redis 'all daily monthly'
 sudo docker exec ori_backend_1 ./manage.py extract list_sources |tail -n +2 |grep ' -s ' |awk '{print $(NF)}' >sources.txt
