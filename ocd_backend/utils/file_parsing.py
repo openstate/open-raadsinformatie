5efa12565111ebfa264c9d236d92bb124c1848d9
Rob van Dijk <697696+robvandijk@users.noreply.github.com>
2025-01-07 09:09:22 +0100

    Adds retry using OCR for empty parse results

--- ocd_backend/utils/file_parsing.py
+++ ocd_backend/utils/file_parsing.py
@@ -1,3 +1,5 @@
+import re
+
 import magic
 import pdftotext
 import pymupdf
@@ -8,7 +10,7 @@ from ocd_backend.log import get_source_logger
 log = get_source_logger('file_parser')
 
 
-def file_parser(fname, max_pages=None):
+def file_parser(fname, original_url, max_pages=None):
     if magic.from_file(fname, mime=True) == 'application/pdf':
         with open(fname, "rb") as f:
             result_pages = []
@@ -20,9 +22,10 @@ def file_parser(fname, max_pages=None):
                     if max_pages and i >= max_pages:  # break after x pages
                         break
             except pdftotext.Error as e:
-                log.warning(str(e))
+                log.warning(f"Error from pdftotext for {original_url}: {e}")
+                raise e
 
-            log.debug("Processed %i pages" % i)
+            log.debug(f"Processed %i pages for {original_url}" % i)
             return result_pages
     else:
         # This code path is disabled until the Tika service is fixed (see issue 178)
@@ -35,14 +38,37 @@ def file_parser(fname, max_pages=None):
         #     raise
         pass
 
-def md_file_parser(fname, max_pages=None):
+def md_file_parser(fname, original_url):
     if magic.from_file(fname, mime=True) == 'application/pdf':
         doc=pymupdf.open(fname)
         hdr=pymupdf4llm.IdentifyHeaders(doc)
         md_chunks = pymupdf4llm.to_markdown(fname, hdr_info=hdr, page_chunks=True, show_progress=False)
         md_text = [chunk['text'] for chunk in md_chunks]
 
+        if parse_result_is_empty(md_text):
+            log.info(f"Parse result is empty for {original_url}, now trying OCR")
+            md_text = md_file_parser_using_ocr(fname)
+
         return md_text
     else:
         pass
 
+def md_file_parser_using_ocr(fname):
+    doc=pymupdf.open(fname)
+    hdr=pymupdf4llm.IdentifyHeaders(doc)
+
+    md_text = []
+    for page in doc:
+        pixmap = page.get_pixmap(dpi=300)
+        doc = pymupdf.open("pdf", pixmap.pdfocr_tobytes(language='nld'))
+        md_text.append(pymupdf4llm.to_markdown(doc, hdr_info=hdr, show_progress=False))
+
+    return md_text
+
+def parse_result_is_empty(md_text):
+    # If at least a single digit or letter is present on a page we consider it not empty  
+    for page in md_text:
+        if re.search("[a-zA-Z0-9]", page):
+            return False
+        
+    return True
