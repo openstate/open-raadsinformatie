2d60c0395a3aac82b218c3b2cdbcdfa7d2e43659
Rob van Dijk <697696+robvandijk@users.noreply.github.com>
2025-02-18 09:57:03 +0100

    Enables parallel processing for multiple suppliers

--- bin/reindex.sh
+++ bin/reindex.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
-# This script will reindex all sources listed in $TO_INDEX_FILENAME. It is
-# run periodically by crontab to see if the next source can be started.
+# This script will reindex all sources listed in $TO_INDEX_FILENAME, and can be run for
+# each supplier separately.
+# Use
+#   bin/reindex
+# without any parameters to process all sources sequentially.
+# Pass a supplier e.g.
+#   bin/reindex notubiz
+# and start multiple processes for parallel processing.
+#
+# Run this script periodically using crontab to check if the next source can be started.
 #
 # It will only start the next source if the lock has been released for the previous.
 #
@@ -12,13 +20,22 @@
 #
 SUDO="sudo " # In development substitute with empty string
 MAINTENANCE_FILE="maintenance.txt"
-LOCK_KEY="ori_lock_key"
-LOCK_TIME_KEY="ori_lock_time_key"
 IS_LOCKED="ori_is_locked"
-MAIL_SENT_KEY="ori_mail_sent"
 
-TO_INDEX_FILENAME='to_index.txt'
-INDEXED_FILENAME='indexed.log'
+SUPPLIERS=(notubiz ibabs go parlaeus)
+SUPPLIER_SUFFIX=""
+for ((i=0; i < ${#SUPPLIERS[@]}; i++)); do
+    if [ "${SUPPLIERS[i]}" == "$1" ]; then
+        SUPPLIER_SUFFIX="_$1"
+    fi
+done
+
+TO_INDEX_FILENAME="to_index${SUPPLIER_SUFFIX}.txt"
+LOCK_KEY="ori_lock_key${SUPPLIER_SUFFIX}"
+LOCK_TIME_KEY="ori_lock_time_key${SUPPLIER_SUFFIX}"
+MAIL_SENT_KEY="ori_mail_sent${SUPPLIER_SUFFIX}"
+
+INDEXED_FILENAME="indexed${SUPPLIER_SUFFIX}.log"
 START_DATE='2010-01-01'
 END_DATE=`date -d "-1 day" +%Y-%m-%d`
 INDEXING_TIME_LIMIT=43200 # half a day in seconds
@@ -28,11 +45,18 @@ BINDIR=`dirname $FQPATH`
 HOMEDIR=`dirname $BINDIR`
 cd $HOMEDIR
 
+# -----------------------------------------------------------------------------------
+# Stop further processing if maintenance file is detected
+# Create maintenance file when desired using `touch maintenance.txt`
+# -----------------------------------------------------------------------------------
 if test -f "$HOMEDIR/$MAINTENANCE_FILE"; then
     echo "Maintenance, bailing out"
     exit
 fi
 
+# -----------------------------------------------------------------------------------
+# Function definitions
+# -----------------------------------------------------------------------------------
 function get_locked_source() {
     echo $($SUDO docker exec ori_redis_1 sh -c "redis-cli -n 1 get $LOCK_KEY")
 }
@@ -86,7 +110,7 @@ function warn_processing_time() {
     EMAIL_TO="developers@openstate.eu"
     FROM_EMAIL="developers@openstate.eu"
     FROM_NAME="Openraadsinformatie Indexing"
-    SUBJECT="Processing $locked_source is taking too long"
+    SUBJECT="Processing $locked_source ($SUPPLIER_SUFFIX) is taking too long"
 
     bodyHTML="<p>Processing started $date_diff seconds ago</p>"
 
@@ -102,6 +126,10 @@ function warn_processing_time() {
     set_mail_sent $locked_source
 }
 
+# -----------------------------------------------------------------------------------
+# Check if previous source is still being processed. If so, bail out. Send warning
+# if processing takes too long.
+# -----------------------------------------------------------------------------------
 LOCKED=$(check_locked)
 if [ "$LOCKED" = "$IS_LOCKED" ]; then
     echo "Indexing busy, not starting new source"
@@ -118,7 +146,9 @@ if [ "$LOCKED" = "$IS_LOCKED" ]; then
     exit
 fi
 
-# Get first entry from TO_INDEX_FILE
+# -----------------------------------------------------------------------------------
+# Get next entry from TO_INDEX_FILE and start processing
+# -----------------------------------------------------------------------------------
 TO_INDEX_FILE="$HOMEDIR/$TO_INDEX_FILENAME"
 SOURCE=`sed -i -e '1 w /dev/stdout' -e '1d' $TO_INDEX_FILE`
 if [ "$SOURCE" != "" ];
