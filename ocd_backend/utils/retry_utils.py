fdff293f76296c1b058d647aa363e8284d8e853b
Rob van Dijk <697696+robvandijk@users.noreply.github.com>
2025-03-17 12:08:45 +0100

    Adds another non-retryable error case

--- ocd_backend/utils/retry_utils.py
+++ ocd_backend/utils/retry_utils.py
@@ -54,7 +54,7 @@ def is_retryable_error(error):
         retryable = False
     if 'NameResolutionError' in error_string:
         retryable = False
-    if 'ConnectTimeoutError' in error_string and 'azavista.com' in error_string:
+    if 'azavista.com' in error_string and ('ConnectTimeoutError' in error_string or 'Connection refused' in error_string):
         retryable = False
     if 'www.mantelzorgpleinalmere.nl' in error_string and 'SSLV3_ALERT_HANDSHAKE_FAILURE' in error_string:
         retryable = False
