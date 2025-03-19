3498a88ef7b4d1b172da480ae9dd21e5629f0aa2
Rob van Dijk <697696+robvandijk@users.noreply.github.com>
2025-03-19 12:12:46 +0100

    Sets max-tasks-per-child for backend

--- bin/start_backend.dev.sh
+++ bin/start_backend.dev.sh
@@ -6,7 +6,7 @@ celery --app ocd_backend.app:celery_app flower &
 #celery --app=ocd_backend.app:celery_app worker --pidfile=/var/run/celery/%n.pid --loglevel=info --concurrency=8
 # nov 2024: using this now to start celery in development to get the logs. The "celery multi" command below may be faster
 # but for some reason does not write to the logs
-celery --app=ocd_backend.app:celery_app --quiet worker --loglevel=debug -Q transformers,enrichers --concurrency=8 --without-gossip >> /opt/ori/data/ori.log 2>&1
+celery --app=ocd_backend.app:celery_app --quiet worker --loglevel=debug -Q transformers,enrichers --concurrency=8 --max-tasks-per-child=5 --without-gossip >> /opt/ori/data/ori.log 2>&1
 # celery -A ocd_backend.app:celery_app multi start 4 -Q transformers,enrichers -c8 -l debug --pidfile=/var/run/celery/%n.pid
 # while true
 #   do
--- bin/start_backend.sh
+++ bin/start_backend.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # When concurrency was 14 machine nearly crashed due to memory full and swap space full
 cd /opt/ori
-celery --app=ocd_backend.app:celery_app --quiet worker --loglevel=info -Q transformers,enrichers --concurrency=7 --without-gossip >> /opt/ori/data/ori.log 2>&1
+celery --app=ocd_backend.app:celery_app --quiet worker --loglevel=info -Q transformers,enrichers --concurrency=7 --max-tasks-per-child=5 --without-gossip >> /opt/ori/data/ori.log 2>&1
