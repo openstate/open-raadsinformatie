6b5af021a3bb970d716840c93b9b67879b612586
Rob van Dijk <697696+robvandijk@users.noreply.github.com>
2025-05-01 08:15:24 +0200

    Adds retry exception for oisterwijk

--- ocd_backend/utils/retry_utils.py
+++ ocd_backend/utils/retry_utils.py
@@ -77,6 +77,8 @@ def is_retryable_error(error, url = None):
         retryable = False
     if 'ConnectTimeoutError' in error_string and '217.149.64.42' in error_string:
         retryable = False
+    if 'www.ouderenombudsman.nl' in error_string:
+        retryable = False
 
     if not retryable:
         log.info(f'Non-retryable error caught ({error.__class__.__name__}):\n{error_string}')
