e62127558b5761f080c7ce644999782c8b31bbd9
Rob van Dijk <697696+robvandijk@users.noreply.github.com>
2025-05-15 08:46:23 +0200

    Adds retry exception for Lansingerland

--- ocd_backend/utils/retry_utils.py
+++ ocd_backend/utils/retry_utils.py
@@ -82,6 +82,8 @@ def is_retryable_error(error, url = None, retries_sofar = None):
         retryable = False
     if 'veenendaalsekrant.nl' in error_string or '.mijnstem.nl' in error_string:
         retryable = False
+    if 'ReadTimeoutError' in error_string and 'da91a75e-f516-4633-809b-39141b875d87' in error_string:
+        retryable = False
 
     # If maximum number of retries has been reached see if we should stop retrying
     if retryable and retries_sofar == RETRY_MAX_RETRIES:
