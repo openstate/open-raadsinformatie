d44a818e45737ec05860c0242d435a131435f133
Rob van Dijk <697696+robvandijk@users.noreply.github.com>
2026-03-26 08:03:12 +0100

    Changes location PostgreSQL backup

--- bin/backup-pg.sh
+++ bin/backup-pg.sh
@@ -3,4 +3,4 @@ DB_CONTAINER="ori_postgres_1"
 DB_NAME="ori"
 DB_USER="postgres"
 
-docker exec $DB_CONTAINER pg_dump $DB_NAME -U $DB_USER | gzip > backups/latest-postgresdump-daily.sql.gz
+docker exec $DB_CONTAINER pg_dump $DB_NAME -U $DB_USER | gzip > /data/backups/latest-postgresdump-daily.sql.gz
